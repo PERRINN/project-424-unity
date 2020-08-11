ac461d2d371efae26c3e321eb929abbbb6e13a96
Jack Teo <61779701+JackTeo@users.noreply.github.com>
2020-08-12 08:21:07 +1200

    update

---
+++ Assets/Render Texture/GhostManager.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using VehiclePhysics;
+
+public struct GhostTransform
+{
+    public Vector3 position;
+    public Quaternion rotation;
+
+    public GhostTransform(Transform transform)
+    {
+        this.position = transform.position;
+        this.rotation = transform.rotation;
+    }
+}
+
+public class GhostManager : MonoBehaviour
+{
+    public Transform vehicle;
+    public Transform ghostVehicle;
+    
+    public bool recording;
+    public bool playing;
+
+    private List<GhostTransform> recordedGhostTransform = new List<GhostTransform>();
+    private GhostTransform lastRecordedGhostFrame;
+
+    private void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (recording)
+        {
+            if (vehicle.position != lastRecordedGhostFrame.position || vehicle.rotation != lastRecordedGhostFrame.rotation)
+            {
+                var newGhostTransform = new GhostTransform(vehicle);
+                recordedGhostTransform.Add(newGhostTransform);
+
+                lastRecordedGhostFrame = newGhostTransform;
+            }
+
+        }
+
+        if (playing)
+        {
+            Play();
+        }
+    }
+
+    void Play()
+    {
+        ghostVehicle.gameObject.SetActive(true);
+        StartCoroutine(StartGhost());
+        playing = false;
+    }
+
+    IEnumerator StartGhost()
+    {
+        for (int i = 0; i < recordedGhostTransform.Count; i++)
+        {
+            ghostVehicle.position = recordedGhostTransform[i].position;
+            ghostVehicle.rotation = recordedGhostTransform[i].rotation;
+            yield return new WaitForFixedUpdate();
+            
+        }
+    }
+}
