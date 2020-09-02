0b12a1e3f08b9381ca5a17a57892945f1ab0dc0e
AlexWebber807 <65652297+AlexWebber807@users.noreply.github.com>
2020-09-02 14:22:04 +1200

    Sound Testing Scene
    This scene contains scripts to increase the pitch and volume based on the increase in RPMS(text) and Torque(text)

---
+++ Assets/Scripts/Under Development/UpdateTextValues.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UpdateTextValues : MonoBehaviour
+{
+    public Slider Slider;
+    public Text RPMS;
+    public Text torque;
+    public Slider torqueSlider;
+
+    void Start()
+    {
+        RPMS = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue()
+    {
+        string sliderMessage = Slider.value.ToString();
+        string tsliderMessage = torqueSlider.value.ToString();
+        torque.text = tsliderMessage;
+        RPMS.text = sliderMessage;
+    }
+}
