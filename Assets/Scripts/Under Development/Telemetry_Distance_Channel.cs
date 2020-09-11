8127fd4e5647717ec3ff2df9da5583c550534a49
Anthony <58295111+Murplepunky@users.noreply.github.com>
2020-09-12 09:30:53 +1200

    Total Distance Telemetry Channel
    Added a new script that calculates the Perrinn424's total distance travelled.

--- Assets/Scripts/Under Development/Telemetry_Distance_Channel.cs
+++ Assets/Scripts/Under Development/Telemetry_Distance_Channel.cs
@@ -2,37 +2,81 @@
 using System.Collections.Generic;
 using UnityEngine;
 using VehiclePhysics;
+using EdyCommonTools;
 
-public class Telemetry_Distance_Channel : MonoBehaviour
+namespace Project424
 {
-    /*
-     https://i.imgur.com/6iPmROn.png
-     https://i.imgur.com/nifyuKv.png
-     https://i.imgur.com/fxdvd73.png
-         */
+#if !VPP_ESSENTIAL
 
-    // Start is called before the first frame update
-    void Start()
+    [RequireComponent(typeof(VPPerformanceDisplay))]
+    public class Telemetry_Distance_Channel : MonoBehaviour
     {
-        
+        // Member variables
+        private Vector3 m_lastPosition;
+        public static float m_totalDistance { get; private set; }
+        public static float m_lapDistance { get; private set; }
+
+        public Rigidbody Perrinn424;
+        public BoxCollider StartLine;
+
+        public enum Charts { TotalDistanceChart };
+
+        // Shows dropdown list in Unity Inspector
+        public Charts chart = Charts.TotalDistanceChart;
+
+        // The array of charts
+        PerformanceChart[] m_charts = new PerformanceChart[]
+        {
+        new TotalDistanceChart()
+        };
+
+        VPPerformanceDisplay m_perfComponent;
+
+        void OnEnable()
+        {   // When enabled, get VPPDisplay and assign member variables
+            m_perfComponent = GetComponent<VPPerformanceDisplay>();
+
+            // Initialises the variables
+            m_lastPosition = Perrinn424.position;
+            m_totalDistance = 0;
+            m_lapDistance = 0;
+        }
+
+        void FixedUpdate()
+        {
+            // Calculates the total distance travelled by subtracting lastPosition from current position
+            float distance = (Perrinn424.position - m_lastPosition).magnitude;
+
+            // Updates lastPosition to current position and increment totalDistance
+            m_lastPosition = Perrinn424.position;
+            m_totalDistance += distance;
+            m_lapDistance += distance;
+            
+            
+
+            // Passes the exposed parameters to their corresponding charts
+            TotalDistanceChart totalDistanceChart = m_charts[(int)Charts.TotalDistanceChart] as TotalDistanceChart;
+
+            // Applies the selected custom chart
+            m_perfComponent.customChart = m_charts[(int)chart];            
+        }
     }
 
-    void FixedUpdate()
-    {
-        
-    }
-
+    // Total Distance Chart
     public class TotalDistanceChart : PerformanceChart
     {
-        public override string Title ()
+        // Creates channel for distance travelled
+        DataLogger.Channel m_totalDistanceTravelled;
+
+        public override string Title()
         {
             return "Total Distance Travelled";
         }
 
         public override void Initialize()
         {
-            dataLogger.topLimit = 12.0f;
-            dataLogger.bottomLimit = -0.5f;
+            dataLogger.topLimit = 1000f;
+            dataLogger.bottomLimit = 0f;
         }
 
         public override void ResetView()
@@ -40,15 +84,22 @@ public class Telemetry_Distance_Channel : MonoBehaviour
             dataLogger.rect = new Rect(0.0f, -0.5f, 30.0f, 12.5f);
         }
 
-        public override void RecordData()
-        {
-            throw new System.NotImplementedException();
-        }
-
         public override void SetupChannels()
         {
-            throw new System.NotImplementedException();
+            m_totalDistanceTravelled = dataLogger.NewChannel("Total Distance");
+            m_totalDistanceTravelled.color = GColor.gray;
+            m_totalDistanceTravelled.SetOriginAndSpan(3.5f, 2.0f, 1.0f);
+            m_totalDistanceTravelled.valueFormat = "0 m";
+            m_totalDistanceTravelled.captionPositionY = 1;
+        }
+
+        public override void RecordData()
+        {
+            //passes the distance to the datalogger to write on the chart
+            m_totalDistanceTravelled.Write(Telemetry_Distance_Channel.m_totalDistance);
+            m_totalDistanceTravelled.SetOriginAndSpan(3.5f, 2.0f, Telemetry_Distance_Channel.m_totalDistance / 3);
         }
     }
 
+#endif
 }
