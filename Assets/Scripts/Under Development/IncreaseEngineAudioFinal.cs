f679fe975a0eea8dbec3dfdc7a637cf07c1f5b15
AlexWebber807 <65652297+AlexWebber807@users.noreply.github.com>
2020-09-18 02:55:11 +1200

    Sounds Applied To Car
    These changes apply the audio changes to the car

---
+++ Assets/Scripts/Under Development/IncreaseEngineAudioFinal.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using VehiclePhysics;
+
+
+public class IncreaseEngineAudioFinal : MonoBehaviour
+{
+    public VehicleBase vehicle;
+    public float increasingValueTRQ;
+    public float increaseingValueRPM;
+    public float basePitch;
+    public float baseVolume;
+    public AudioSource engineAudio;
+
+    float frontRpm;
+    float frontMechanical;
+    float rearRpm;
+    float rearMechanical;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        int[] custom = vehicle.data.Get(Channel.Custom);
+        frontRpm = custom[Perrinn424Data.FrontMguBase + Perrinn424Data.Rpm] / 1000.0f;
+        frontMechanical = custom[Perrinn424Data.FrontMguBase + Perrinn424Data.MechanicalTorque] / 1000.0f;
+        rearRpm = custom[Perrinn424Data.RearMguBase + Perrinn424Data.Rpm] / 1000.0f;
+        rearMechanical = custom[Perrinn424Data.RearMguBase + Perrinn424Data.MechanicalTorque] / 1000.0f;
+
+        //print(frontMechanical);
+
+        UpdateFrontEngine();
+        UpdateRearEngine();
+    }
+
+    public void UpdateFrontEngine()
+    {
+        engineAudio.pitch = basePitch + Mathf.Abs(frontRpm) * increaseingValueRPM;
+        engineAudio.volume = baseVolume + Mathf.Abs(frontMechanical) * increasingValueTRQ;
+    }
+
+    public void UpdateRearEngine()
+    {
+        engineAudio.pitch = basePitch + Mathf.Abs(rearRpm) * increaseingValueRPM;
+        engineAudio.volume = baseVolume + Mathf.Abs(rearMechanical) * increasingValueTRQ;
+    }
+}
