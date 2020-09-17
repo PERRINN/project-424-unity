9a43b2562e373a3d6e00cb8f5506d2953a58a1c5
Jack Teo <61779701+JackTeo@users.noreply.github.com>
2020-09-18 10:49:03 +1200

    PID Update
    NaN bug error solved.
    Error distances changes between positive and negative value.
    Added PID Telemetry

--- Assets/PID/PID.cs
+++ Assets/PID/PID.cs
@@ -17,17 +17,21 @@ public class PID : MonoBehaviour
 
     List<VPReplay.Frame> recordedReplay = new List<VPReplay.Frame>();
 
-    float height;
+    int cuts;
+    public static float height = 0;
 
     void Start()
     {
         recordedReplay = replayController.predefinedReplay.recordedData;
+        cuts = recordedReplay.Count / 500;
     }
 
     void Update()
     {
-        getDistance();
-
+        if (recordedReplay.Count >= cuts)
+        {
+            getDistance();
+        }
     }
 
     void OnGUI()
@@ -44,10 +48,9 @@ public class PID : MonoBehaviour
         float currentPosX = target.recordedData[currentFrame].position.x;
         float currentPosZ = target.recordedData[currentFrame].position.z;
 
+        int sectionSize = recordedReplay.Count / cuts;
 
-        int sectionSize = recordedReplay.Count / 100;
-
-        float minDistance = float.MaxValue;
+        float minDistance1 = float.MaxValue;
         float minDistance2 = float.MaxValue;
 
         int frame1 = 0;
@@ -55,19 +58,19 @@ public class PID : MonoBehaviour
         int frame3 = 0;
         int frame4 = 0;
 
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < cuts; i++)
         {
             float x = recordedReplay[sectionSize * i].position.x - currentPosX;
             float z = recordedReplay[sectionSize * i].position.z - currentPosZ;
 
             float distance = (float)Math.Sqrt((x * x) + (z * z));
 
-            if (distance < minDistance)
+            if (distance < minDistance1)
             {
                 frame2 = frame1;
                 frame1 = sectionSize * i;
-                minDistance2 = minDistance;
-                minDistance = distance;
+                minDistance2 = minDistance1;
+                minDistance1 = distance;
             }
             else if (distance < minDistance2)
             {
@@ -84,19 +87,25 @@ public class PID : MonoBehaviour
             frame2 = temp;
         }
 
-        for (int i = frame1; i < frame2; i++)
+        minDistance1 = float.MaxValue;
+        minDistance2 = float.MaxValue;
+
+        frame1 = (frame1 - 50 <= 0) ? 0 : frame1 -= 50;
+        frame2 = (frame2 + 100 >= recordedReplay.Count - 1) ? recordedReplay.Count - 1 : frame2 += 100;
+
+        for (int i = frame1; i <= frame2; i++)
         {
             float x = recordedReplay[i].position.x - currentPosX;
             float z = recordedReplay[i].position.z - currentPosZ;
 
             float distance = (float)Math.Sqrt((x * x) + (z * z));
 
-            if (distance < minDistance)
+            if (distance < minDistance1)
             {
                 frame4 = frame3;
                 frame3 = i;
-                minDistance2 = minDistance;
-                minDistance = distance;
+                minDistance2 = minDistance1;
+                minDistance1 = distance;
             }
             else if (distance < minDistance2)
             {
@@ -108,11 +117,19 @@ public class PID : MonoBehaviour
         float a = recordedReplay[frame3].position.x - recordedReplay[frame4].position.x;
         float b = recordedReplay[frame3].position.z - recordedReplay[frame4].position.z;
         float minDistance3 = (float)Math.Sqrt((a * a) + (b * b));
+        float s = (minDistance1 + minDistance2 + minDistance3) / 2;
+        float area = (float)Math.Sqrt(s * (s - minDistance1) * (s - minDistance2) * (s - minDistance3));
 
-        float s = (minDistance + minDistance2 + minDistance3) / 2;
+        float abcX = recordedReplay[frame3].position.x - currentPosX;
+        float abcZ = recordedReplay[frame3].position.z - currentPosZ;
+        float degree = -(float)(Math.PI * recordedReplay[frame3].rotation.eulerAngles.y / 180);
+        float cosD = (float)Math.Cos(degree);
+        float sinD = (float)Math.Sin(degree);
+        float abcXX = (abcX * cosD) + (abcZ * sinD);
 
-        float area = (float)Math.Sqrt(s * (s - minDistance) * (s - minDistance2) * (s - minDistance3));
-        height = area * 2 / minDistance3;
+        float checkHeight = area * 2 / minDistance3;
+
+        height = (abcXX > 0) ? checkHeight : -checkHeight;
 
         if (showPosition)
         {
@@ -120,15 +137,5 @@ public class PID : MonoBehaviour
             cubeTwo.transform.position = recordedReplay[frame4].position;
             cubeCar.transform.position = target.recordedData[currentFrame].position;
         }
-
-
-        //print("Frame 1: " + frame3 + "   Frame 2: " + frame4);
-        //print("Car position: " + target.recordedData[currentFrame].position);
-        ////print("Dummy1 position: " + recordedReplay[frame3].position);
-        ////print("Dummy2 position: " + recordedReplay[frame4].position);
-        //print("Car - Dummy1 distance: " + minDistance);
-        //print("Car - Dummy2 distance: " + minDistance2);
-        //print("Dummy1 - Dummy2 distance: " + minDistance3);
-        //print("Height: " + height);
     }
 }
