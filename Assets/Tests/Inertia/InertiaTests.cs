fcea02370ec01968ddb6970282e4608bebb7d6c4
Edy <edytado@gmail.com>
2020-09-28 16:31:26 +0200

    Inertia test scene and script

---
+++ Assets/Tests/Inertia/InertiaTests.cs
@@ -0,0 +1,119 @@
+
+
+using UnityEngine;
+using VehiclePhysics;
+using EdyCommonTools;
+
+
+[RequireComponent(typeof(Rigidbody))]
+public class InertiaTests : MonoBehaviour
+	{
+	public Inertia.Settings inertia = new Inertia.Settings();
+
+	[Header("Force Application")]
+	public Vector3 forcePosition = Vector3.right;
+	public Vector3 forceVector = Vector3.forward;
+	public float forceStart = 1.0f;
+	public float forceDuration = 1.0f;
+
+	[Header("Conditions")]
+	public float deltaTime = 0.005f;
+
+	[Header("Display")]
+	public Vector2 position = new Vector2(8, 8);
+	public Font font;
+	[Range(6,100)]
+	public int fontSize = 17;
+	public Color fontColor = Color.white;
+
+
+	// Private members
+
+
+	Rigidbody m_rigidbody;
+	Inertia m_inertiaHelper = new Inertia();
+	float m_internalTime;
+
+	string m_results;
+	GUIStyle m_textStyle = new GUIStyle();
+	float m_boxWidth;
+	float m_boxHeight;
+
+
+	// Component methods
+
+
+	void OnEnable ()
+		{
+		m_rigidbody = GetComponent<Rigidbody>();
+
+		m_inertiaHelper.settings = inertia;
+		m_inertiaHelper.Apply(m_rigidbody);
+
+		Time.fixedDeltaTime = deltaTime;
+		m_internalTime = 0.0f;
+
+		m_results = "";
+		}
+
+
+	void Update ()
+		{
+		m_textStyle.font = font;
+		m_textStyle.fontSize = fontSize;
+		m_textStyle.normal.textColor = fontColor;
+
+		m_inertiaHelper.DoUpdate(m_rigidbody);
+		}
+
+
+	void FixedUpdate ()
+		{
+		// Apply the force during the specified time
+
+		if (m_internalTime >= forceStart && m_internalTime < forceStart + forceDuration)
+			{
+			ApplyForce();
+			}
+
+		// Retrieve results
+
+
+
+		// Advance physics time
+
+		m_internalTime += deltaTime;
+		m_internalTime = MathUtility.RoundDecimals(m_internalTime, 3);
+		}
+
+
+	void ApplyForce ()
+		{
+		m_rigidbody.AddForceAtPosition(forceVector, m_rigidbody.worldCenterOfMass + forcePosition);
+		}
+
+
+	void OnGUI ()
+		{
+		// Compute box size
+
+		Vector2 contentSize = m_textStyle.CalcSize(new GUIContent(m_results));
+		float margin = m_textStyle.lineHeight * 1.2f;
+		float headerHeight = GUI.skin.box.lineHeight;
+
+		m_boxWidth = contentSize.x + margin;
+		m_boxHeight = contentSize.y + headerHeight + margin / 2;
+
+		// Compute box position
+
+		float xPos = position.x < 0? Screen.width + position.x - m_boxWidth : position.x;
+		float yPos = position.y < 0? Screen.height + position.y - m_boxHeight : position.y;
+
+		// Draw telemetry box
+
+		GUI.Box(new Rect (xPos, yPos, m_boxWidth, m_boxHeight), "Inertia Results");
+		GUI.Label(new Rect (xPos + margin / 2, yPos + margin / 2 + headerHeight, Screen.width, Screen.height), m_results, m_textStyle);
+		}
+
+
+	}
