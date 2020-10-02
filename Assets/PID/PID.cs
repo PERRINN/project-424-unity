d6a21227c86b47117df8f4677b0b23a82d12197d
Edy <edytado@gmail.com>
2020-10-02 22:56:51 +0200

    Force feedback disabled on autopilot mode

--- Assets/PID/PID.cs
+++ Assets/PID/PID.cs
@@ -34,21 +34,46 @@ public class PID : MonoBehaviour
     int frame2;
     bool runOnce = false;
 
-    void Start()
+    VPDeviceInput m_deviceInput;
+    float m_ffbForceIntensity;
+    float m_ffbDamperCoefficient;
+
+    void OnEnable()
     {
         recordedReplay = replayController.predefinedReplay.recordedData;
         cuts = recordedReplay.Count / 500;
+
+        m_deviceInput = vehicleBase.GetComponentInChildren<VPDeviceInput>();
+        if (m_deviceInput != null)
+        {
+            m_ffbForceIntensity = m_deviceInput.forceIntensity;
+            m_ffbDamperCoefficient = m_deviceInput.damperCoefficient;
+        }
     }
 
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.Q))
         {
-            if (autopilotON) { autopilotON = false; }
+            if (autopilotON)
+            {
+                autopilotON = false;
+                if (m_deviceInput != null)
+                {
+                    m_deviceInput.forceIntensity = m_ffbForceIntensity;
+                    m_deviceInput.damperCoefficient = m_ffbDamperCoefficient;
+                }
+            }
             else
             {
                 autopilotON = true;
                 edyPID.Reset();
+
+                if (m_deviceInput != null)
+                {
+                    m_deviceInput.forceIntensity = 0.0f;
+                    m_deviceInput.damperCoefficient = 0.0f;
+                }
             }
         }
     }
