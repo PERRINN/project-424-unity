a029654e0d89b00e9b1e74c781f17eacea7197ea
Augusto Dufloth <dufloth@gmail.com>
2020-10-16 23:17:08 -0300

    Removed unnecessary code from Aerodynamics

--- Assets/Scripts/Perrinn424Aerodynamics.cs
+++ Assets/Scripts/Perrinn424Aerodynamics.cs
@@ -7,18 +7,6 @@ using System.Collections;
 // bodyRoll: Change to the correct vehicle body roll due to suspension kinematics
 public class Perrinn424Aerodynamics : VehicleBehaviour
 {
-	public bool showTelemetry = false;
-	public Vector2 position = new Vector2(8, 8);
-	public Font font;
-	[Range(6, 100)]
-	public int fontSize = 10;
-	public Color fontColor = Color.white;
-
-	string m_text = "";
-	GUIStyle m_textStyle = new GUIStyle();
-	float m_boxWidth;
-	float m_boxHeight;
-
 	[Serializable]
 
 	public class AeroSettings
@@ -26,33 +14,32 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 		public Transform applicationPoint;
 
 		// Coefficients for the aero model equation
-		public float constant = 1.0f;
-		public float frontRideHeightCoefficient = 1.0f;
+		public float constant                    = 1.0f;
+		public float frontRideHeightCoefficient  = 1.0f;
 		public float frontRideHeight2Coefficient = 1.0f;
-		public float rearRideHeightCoefficient = 1.0f;
-		public float rearRideHeight2Coefficient = 1.0f;
-		public float absoluteYawCoefficient = 1.0f;
-		public float absoluteSteerCoefficient = 1.0f;
-		public float absoluteRollCoefficient = 1.0f;
-		public float dRS_Coefficient = 1.0f;
-		public float frontFlapCoefficient = 1.0f;
+		public float rearRideHeightCoefficient   = 1.0f;
+		public float rearRideHeight2Coefficient  = 1.0f;
+		public float absoluteYawCoefficient      = 1.0f;
+		public float absoluteSteerCoefficient    = 1.0f;
+		public float absoluteRollCoefficient     = 1.0f;
+		public float dRS_Coefficient             = 1.0f;
+		public float frontFlapCoefficient        = 1.0f;
 	}
 
-	// creating public dummies - These should be fed by the physics and system inputs
-	public float airDensity = 1.0f;
-	public float frontRideHeight = 1.0f;
-	public float rearRideHeight = 1.0f;
-	public float flapAngle = 1.0f;
+	public float airDensity         = 1.0f;
+	public float frontRideHeight    = 1.0f;
+	public float rearRideHeight     = 1.0f;
+	public float flapAngle          = 1.0f;
 	public float dRSActivationDelay = 1.0f;
-	public float dRSActivationTime = 1.0f;
+	public float dRSActivationTime  = 1.0f;
 
 	// creating public aero instances
 	public AeroSettings front = new AeroSettings();
-	public AeroSettings rear = new AeroSettings();
-	public AeroSettings drag = new AeroSettings();
+	public AeroSettings rear  = new AeroSettings();
+	public AeroSettings drag  = new AeroSettings();
 
 	float DRStime    = 0;
-	bool DRSStatus   = false;
+    bool DRSStatus   = false;
 	bool DRSclosing  = false;
 	public float DRS = 0;
 
@@ -61,12 +48,12 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 	public float SCx      = 0;
 
 	public float downforceFront = 0;
-	public float downforceRear = 0;
-	public float dragForce = 0;
+	public float downforceRear  = 0;
+	public float dragForce      = 0;
 
-	public float yawAngle = 0;
+	public float yawAngle   = 0;
 	public float steerAngle = 0;
-	public float rollAngle = 0;
+	public float rollAngle  = 0;
 
 	//%  Function Name: CalcAeroCoeff 
 	//%  This function calculates a given aerodynamic coefficient based on:
@@ -162,11 +149,6 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 
 	public override void FixedUpdateVehicle()
 	{
-		// Setting telmetry window properties
-		m_textStyle.font = font;
-		m_textStyle.fontSize = fontSize;
-		m_textStyle.normal.textColor = fontColor;
-
 		// Assisgning local variables
 		Rigidbody rb = vehicle.cachedRigidbody;
 		float vSquared        = rb.velocity.sqrMagnitude;
@@ -211,54 +193,11 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 		}
 
 		downforceFront = (float)(SCzFront * dynamicPressure / 9.80665);
-		downforceRear = (float)(SCzRear * dynamicPressure / 9.80665);
-		dragForce = (float)(SCx * dynamicPressure / 9.80665);
+		downforceRear  = (float)(SCzRear  * dynamicPressure / 9.80665);
+		dragForce      = (float)(SCx      * dynamicPressure / 9.80665);
 
-		if (showTelemetry)
-        {
-			m_text = "";
-			m_text += $"SCz Front       : {SCzFront, 6:0.000}\n";
-			m_text += $"SCz Rear        : {SCzRear, 6:0.000}\n";
-			m_text += $"SCx             : {SCx, 6:0.000}\n";
-			m_text += $"Front Downforce : {SCzFront * dynamicPressure / 9.80665, 6:0} kg\n";
-			m_text += $"Rear Downforce  : {SCzRear * dynamicPressure / 9.80665, 6:0} kg\n";
-			m_text += $"Drag Force      : {SCx * dynamicPressure / 9.80665, 6:0} kg\n";
-			m_text += $"DRS logic       : {DRSStatus, 6:0.000}\n";
-			m_text += $"DRS position    : {DRS, 6:0.000}\n";
-			m_text += $"Ride height F	: {frontRideHeight,6:0.0} mm\n";
-			m_text += $"Ride height R	: {rearRideHeight,6:0.0} mm\n";
-			m_text += $"Yaw angle abs	: {yawAngle,6:0.0} deg\n";
-			m_text += $"Steer angle abs	: {steerAngle,6:0.0} deg\n";
-			m_text += $"Roll angle abs	: {rollAngle,6:0.0} deg\n";
-		}
-			
-		
 	}
 
-	void OnGUI()
-	{
-		if (showTelemetry)
-        {
-			// Compute box size
-
-			Vector2 contentSize = m_textStyle.CalcSize(new GUIContent(m_text));
-			float margin = m_textStyle.lineHeight * 1.2f;
-			float headerHeight = GUI.skin.box.lineHeight;
-
-			m_boxWidth = contentSize.x + margin;
-			m_boxHeight = contentSize.y + headerHeight + margin / 2;
-
-			// Compute box position
-
-			float xPos = position.x < 0 ? Screen.width + position.x - m_boxWidth : position.x;
-			float yPos = position.y < 0 ? Screen.height + position.y - m_boxHeight : position.y;
-
-			// Draw telemetry box
-
-			GUI.Box(new Rect(xPos, yPos, m_boxWidth, m_boxHeight), "424 Aerodynamics");
-			GUI.Label(new Rect(xPos + margin / 2, yPos + margin / 2 + headerHeight, Screen.width, Screen.height), m_text, m_textStyle);
-		}
-	}
 }
 
 
