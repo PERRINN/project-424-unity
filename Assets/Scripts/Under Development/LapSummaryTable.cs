fcfcb23e3e5e8eb4584f422a96ea675e7affee27
Anthony <anthonywarrington@hotmail.com>
2020-10-31 09:42:18 +1300

    Fixed issue...
    Fixed an issue where the invalid lap indicator would not show on the first lap if it was an invalid lap.
    Also set lap counter to 1 instead of the counter being changed to the size of m_laps list in OnEnable() method.

--- Assets/Scripts/Under Development/LapSummaryTable.cs
+++ Assets/Scripts/Under Development/LapSummaryTable.cs
@@ -68,8 +68,7 @@ namespace Project424
             //m_laps.Add(new Lap(8, 55f, 99f, 78f, 90.41231f, false));
             //m_laps.Add(new Lap(9, 22f, 15f, 12f, 91.41231f, false));
             //m_laps.Add(new Lap(101, 40f, 76f, 32f, 92.41231f, false));
-
-            m_lapCount = m_laps.Count();
+            //m_lapCount = m_laps.Count();
 
             UpdateLapTable(m_laps);
         }
@@ -128,7 +127,7 @@ namespace Project424
                 m_text += ("   Lap " + lap.LapNum).PadRight(11, ' ');
 
                 // If its the first lap
-                if (prevIndex == 0)
+                if (prevIndex == 0 && lap.IsValid == true)
                 {
                     m_text +=
                         " | G " + FormatSectorTime(lap.Sector1) +
@@ -136,7 +135,15 @@ namespace Project424
                         " | G " + FormatSectorTime(lap.Sector3) +
                         " | G" + FormatLapTime(lap.TotalLapTime) +
                         "\n";
-                }
+                } else if (prevIndex == 0 && lap.IsValid == false) 
+                {
+                    m_text +=
+                        " | G " + FormatSectorTime(lap.Sector1) +
+                        " | G " + FormatSectorTime(lap.Sector2) +
+                        " | G " + FormatSectorTime(lap.Sector3) +
+                        " | G" + FormatLapTime(lap.TotalLapTime) +
+                        "**\n";
+                }                
 
                 // Else check current sectors against prev sectors         
                 else
