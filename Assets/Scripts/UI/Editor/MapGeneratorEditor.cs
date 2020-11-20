3c3364c5e88624a873264df3af84d67bd5561653
Manuel Espino <espinolaramanuel@gmail.com>
2020-11-20 20:07:49 +0100

    - MapGenerator

---
+++ Assets/Scripts/UI/Editor/MapGeneratorEditor.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using EdyCommonTools;
+using EdyCommonTools.EditorTools;
+using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(MapGenerator))]
+public class MapGeneratorEditor : Editor
+{
+    TextureCanvas m_graph = null;
+    const int m_graphWidth = 400;
+    const int m_graphHeight = 400;
+
+    private MapGenerator map;
+
+    private void OnEnable()
+    {
+        map  = (MapGenerator)target;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        Rect graphRect = EditorGUILayout.GetControlRect(false, m_graphHeight);
+
+        GUIDrawGraph(ref m_graph, graphRect, m_graphWidth, m_graphHeight);
+    }
+
+    void GUIDrawGraph(ref TextureCanvas graph, Rect position, int graphWidth, int graphHeight)
+    {
+        if (graph == null || CommonEditorTools.GUIChanged())
+        {
+            if (graph == null)
+            {
+                graph = new TextureCanvas(graphWidth, graphHeight);
+                graph.alpha = 0.75f;
+                graph.color = GColor.black;
+                graph.Clear();
+                graph.Save();
+            }
+            else
+            {
+                graph.Restore();
+            }
+        }
+
+        //float x = -1500;
+        //float y = x;
+        //float size = 2500f;
+        //graph.rect = new Rect(x, y, size, size);
+        Rect r = map.rect;
+        graph.rect = r;
+
+
+        graph.color = Color.white;
+        graph.Line(r.x, r.y, r.x+r.width, r.y+r.height);
+        Vector3[] points = map.points;
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Vector3 pointA = points[i];
+            Vector3 pointB = points[i + 1];
+            graph.Line(pointA.x, pointA.z, pointB.x, pointB.z);
+        }
+
+        TextureCanvasEditor.InspectorDraw(graph, position);
+    }
+}
---
+++ Assets/Scripts/UI/Runtime/MapGenerator.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using VehiclePhysics;
+
+[ExecuteInEditMode]
+public class MapGenerator : MonoBehaviour
+{
+    public VPReplayAsset replay;
+    public float timeStep = 0.05f;
+
+    public Vector3 [] points = new Vector3[0];
+    public Rect rect;
+
+
+    private void Awake()
+    {
+        rect = new Rect(float.PositiveInfinity, float.PositiveInfinity, float.NegativeInfinity, float.NegativeInfinity);
+        Create();
+    }
+
+    [ContextMenu("Force")]
+    private void Create()
+    {
+        timeStep = Mathf.Max(timeStep, replay.timeStep);
+        int step = (int)(timeStep / replay.timeStep);
+        int newLength = replay.recordedData.Count / step;
+        points = new Vector3[newLength];
+
+        float minX, minZ, maxX, maxZ;
+        minX = minZ = float.PositiveInfinity;
+        maxX = maxZ = float.NegativeInfinity;
+
+        for (int i = 0; i < newLength; i++)
+        {
+            points[i] = replay.recordedData[i * step].position;
+
+            Vector3 test = points[i];
+
+            if (test.x < minX)
+                minX = test.x;
+
+            if (test.z < minZ)
+                minZ = test.z;
+
+            if (test.x > maxX)
+                maxX = test.x;
+
+            if (test.z > maxZ)
+                maxZ = test.z;
+        }
+
+        rect = new Rect(minX, minZ, maxX-minX, maxZ-minZ);
+
+        float origin = Mathf.Min(rect.x, rect.y);
+        float size = Mathf.Max(rect.width, rect.height);
+        float augmentedSize = size * 0.1f;
+
+        size += augmentedSize;
+        origin -= augmentedSize*0.5f;
+        rect = new Rect(origin, origin, size, size);
+    }
+
+    private void OnDrawGizmos()
+    {
+        if(replay == null)
+            return;
+
+        for (int i = 0; i < points.Length - 1; i++)
+        {
+            Gizmos.DrawLine(points[i], points[i+1]);
+        }
+        //foreach (Vector3 frame in points)
+        //{
+        //    Gizmos.DrawSphere(frame, 0.1f);
+        //}
+    }
+}
