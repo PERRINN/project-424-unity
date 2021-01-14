f84901ce950da9b79bcf1e0f61e74bc123387a49
Manuel Espino <espinolaramanuel@gmail.com>
2021-01-14 08:13:41 +0100

    - Time Reference Test

---
+++ Assets/Features/UI Design/Scripts/Tests/Editor/TimeReferenceTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+
+namespace Perrinn424.Editor.Tests
+{
+    public class TimeReferenceTests
+    {
+        [Test]
+        public void TimeReferenceTest()
+        {
+            TimeReference porsche = TimeReferenceHelper.CreatePorsche();
+            float diff = porsche.LapDiff(0.1f, 12f);
+            Assert.AreEqual(19f, diff);
+        }
+
+    } 
+}
