d8c47218e2fc3f41c92dfc18e58cc533276c1c1a
Manuel Espino <espinolaramanuel@gmail.com>
2021-01-14 08:29:53 +0100

    - More Tests

--- Assets/Features/UI Design/Scripts/Tests/Editor/TimeReferenceTests.cs
+++ Assets/Features/UI Design/Scripts/Tests/Editor/TimeReferenceTests.cs
@@ -4,12 +4,14 @@ namespace Perrinn424.Editor.Tests
 {
     public class TimeReferenceTests
     {
-        [Test]
-        public void TimeReferenceTest()
+        [TestCase(13.41127f, 657.8914f, 0.4128628f)]
+        [TestCase(24.60585f, 1391.423f, -1.513094f)]
+        [TestCase(5.562164f, 544.5441f, -5.779054f)]
+        public void TimeReferenceTest(float time, float distance, float expectedDifference)
         {
             TimeReference porsche = TimeReferenceHelper.CreatePorsche();
-            float diff = porsche.LapDiff(0.1f, 12f);
-            Assert.AreEqual(19f, diff);
+            float difference = porsche.LapDiff(time, distance);
+            Assert.That(expectedDifference, Is.EqualTo(difference).Within(10e-3));
         }
 
     } 
