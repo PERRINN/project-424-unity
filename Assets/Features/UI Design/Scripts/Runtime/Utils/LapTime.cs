ff6305fe6543f74e3e0b6140f6f46e22472e8860
Manuel Espino <espinolaramanuel@gmail.com>
2021-01-20 09:38:25 +0100

    LapTime

---
+++ Assets/Features/UI Design/Scripts/Runtime/Utils/LapTime.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+
+namespace Perrinn424
+{
+    public class LapTime : IEnumerable
+    {
+        private readonly float[] sectors;
+        public float Total { get; }
+
+        public LapTime(float[] s)
+        {
+            this.sectors = new float[s.Length];
+            Array.Copy(s, sectors, s.Length);
+            Total = s.Sum();
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return sectors.GetEnumerator();
+        }
+
+        public float this[int i] => sectors[i];
+    } 
+}
---
+++ Assets/Features/UI Design/Scripts/Tests/Editor/LapTests.cs
@@ -0,0 +1,38 @@
+﻿using NUnit.Framework;
+
+namespace Perrinn424.Editor.Tests
+{
+    public class LapTests
+    {
+        [Test]
+        public void IEnumerableTest()
+        {
+            float[] sectors = {0f, 1f, 2f};
+            LapTime lapTime = new LapTime(sectors);
+
+            CollectionAssert.AreEquivalent(sectors, lapTime);
+
+            sectors[0] = 5f;
+            CollectionAssert.AreNotEquivalent(sectors, lapTime);
+        }
+
+        [Test]
+        public void TotalTest()
+        {
+            float[] sectors = { 0f, 1f, 2f ,3f, 4f};
+            LapTime lapTime = new LapTime(sectors);
+
+            Assert.AreEqual(10f, lapTime.Total);
+        }
+
+        [Test]
+        public void IndexerTest()
+        {
+            float[] sectors = { 10f, 1f, 2f, 3f, 4f };
+            LapTime lapTime = new LapTime(sectors);
+
+            Assert.AreEqual(10f, lapTime[0]);
+            Assert.AreEqual(2f, lapTime[2]);
+        }
+    } 
+}
