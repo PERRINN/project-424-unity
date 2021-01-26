d5c31de46df57e25c1cb61291ee6df732c6b6d50
Edy <edytado@gmail.com>
2021-01-26 17:42:25 +0100

    Private class for runtime data

--- Assets/Scripts/Perrinn424Underfloor.cs
+++ Assets/Scripts/Perrinn424Underfloor.cs
@@ -23,14 +23,6 @@ public class Perrinn424Underfloor : VehicleBehaviour
 		public float maxLength = 0.05f;
 		[Tooltip("Length above the Point Base for detecting the contact (m). Contact is not detected above this lenght (i.e. on top of the car)")]
 		public float detectionLength = 0.2f;
-
-		// Runtime data
-
-		[NonSerialized] public bool contact;
-		[NonSerialized] public int contactCount;
-		[NonSerialized] public float contactLength;
-		[NonSerialized] public Vector3 verticalForce;
-		[NonSerialized] public Vector3 dragForce;
 		}
 
 	[Tooltip("Points of contact with the ground")]
@@ -51,6 +43,23 @@ public class Perrinn424Underfloor : VehicleBehaviour
 
 	// Trick to assign a default font to the GUI box. Configure it at the script settings in the Editor.
 	[HideInInspector] public Font defaultFont;
+
+
+	// Runtime data
+
+	class ContactPointData
+		{
+		public ContactPoint contactPoint;
+
+		public bool contact;
+		public int contactCount;
+		public float contactLength;
+		public Vector3 verticalForce;
+		public Vector3 dragForce;
+		}
+
+
+	ContactPointData[] m_contactPointData = new ContactPointData[0];
 
 
 	// Initialize the values of new members of the array when its size changes
@@ -104,13 +113,19 @@ public class Perrinn424Underfloor : VehicleBehaviour
 
 	public override void FixedUpdateVehicle ()
 		{
+		if (contactPoints.Length != m_contactPointData.Length)
+			InitializeRuntimeData();
+
+		// Process contact points
+
 		for (int i = 0, c = contactPoints.Length; i < c; i++)
-			ProcessContactPoint(contactPoints[i]);
+			ProcessContactPoint(m_contactPointData[i]);
 		}
 
 
-	void ProcessContactPoint (ContactPoint cp)
+	void ProcessContactPoint (ContactPointData cpData)
 		{
+		ContactPoint cp = cpData.contactPoint;
 		if (cp.pointBase == null)
 			return;
 
@@ -123,10 +138,10 @@ public class Perrinn424Underfloor : VehicleBehaviour
 		if (!Physics.Raycast(origin, -up, out hitInfo, cp.detectionLength, groundLayers, QueryTriggerInteraction.Ignore))
 			return;
 
-		// This is cleared when the widget has acknowledged the contact
+		// This is cleared when the widget has displayed the contact
 
-		cp.contact = true;
-		cp.contactCount++;
+		cpData.contact = true;
+		cpData.contactCount++;
 
 		// Determine if this contact makes sense (i.e. ignore contacts against vertical surfaces)
 
@@ -140,31 +155,44 @@ public class Perrinn424Underfloor : VehicleBehaviour
 		if (contactLength > cp.maxLength)
 			contactLength = cp.maxLength;
 
-		cp.contactLength = contactLength;
+		cpData.contactLength = contactLength;
 
 		// Calculate vertical force
 
 		float verticalLoad = contactLength * cp.stiffness * upNormal;
-		cp.verticalForce = verticalLoad * up;
+		cpData.verticalForce = verticalLoad * up;
 
 		// Calculate longitudinal force
 
 		Vector3 velocity = vehicle.cachedRigidbody.GetPointVelocity(hitInfo.point);
 		Vector3 slipDirection = Vector3.ProjectOnPlane(velocity, hitInfo.normal).normalized;
-		cp.dragForce = -verticalLoad * cp.friction * slipDirection;
+		cpData.dragForce = -verticalLoad * cp.friction * slipDirection;
 
 		// Apply resulting forces
 
-		vehicle.cachedRigidbody.AddForceAtPosition(cp.verticalForce + cp.dragForce, hitInfo.point);
+		vehicle.cachedRigidbody.AddForceAtPosition(cpData.verticalForce + cpData.dragForce, hitInfo.point);
+		}
+
+
+	void InitializeRuntimeData ()
+		{
+		m_contactPointData = new ContactPointData[contactPoints.Length];
+
+		for (int i=0, c = m_contactPointData.Length; i < c; i++)
+			{
+			ContactPointData cp = new ContactPointData();
+			cp.contactPoint = contactPoints[i];
+			m_contactPointData[i] = cp;
+			}
 		}
 
 
 	void ClearContacts ()
 		{
-		foreach (ContactPoint cp in contactPoints)
+		foreach (ContactPointData cpData in m_contactPointData)
 			{
-			cp.contact = false;
-			cp.contactCount = 0;
+			cpData.contact = false;
+			cpData.contactCount = 0;
 			}
 		}
 
@@ -180,19 +208,20 @@ public class Perrinn424Underfloor : VehicleBehaviour
 			m_text.Append($"               Stiffnes  Friction  Events    Depth      Fz    Drag\n");
 			m_text.Append($"                   N/mm         μ               mm       N       N");
 			for (int i = 0, c = contactPoints.Length; i < c; i++)
-				AppendContactPointText(m_text, contactPoints[i]);
+				AppendContactPointText(m_text, m_contactPointData[i]);
 			m_textBox.UpdateText(m_text.ToString());
 			}
 		}
 
 
-	void AppendContactPointText (StringBuilder text, ContactPoint cp)
+	void AppendContactPointText (StringBuilder text, ContactPointData cpData)
 		{
+		ContactPoint cp = cpData.contactPoint;
 		string name = cp.pointBase != null? cp.pointBase.name : "(unused)";
-		string contact = cp.contact? "■" : " ";
+		string contact = cpData.contact? "■" : " ";
 		text.Append($"\n{name,-16} {cp.stiffness/1000.0f,6:0.}    {cp.friction,6:0.00} ");
-		text.Append($"{cp.contactCount,7}{contact} {cp.contactLength*1000.0f,7:0.00} {cp.verticalForce.magnitude,7:0.} {cp.dragForce.magnitude,7:0.}");
-		cp.contact = false;
+		text.Append($"{cpData.contactCount,7}{contact} {cpData.contactLength*1000.0f,7:0.00} {cpData.verticalForce.magnitude,7:0.} {cpData.dragForce.magnitude,7:0.}");
+		cpData.contact = false;
 		}
 
 
