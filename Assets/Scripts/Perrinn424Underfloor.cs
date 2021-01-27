0fb1040310bc86a1ee67a67e38daa4e1f3109984
Edy <edytado@gmail.com>
2021-01-27 21:25:44 +0100

    Low-speed fade out. Speed-based pitch.

--- Assets/Scripts/Perrinn424Underfloor.cs
+++ Assets/Scripts/Perrinn424Underfloor.cs
@@ -44,6 +44,16 @@ public class Perrinn424Underfloor : VehicleBehaviour
 	[Tooltip("Volume at the limit contact depth")]
 	public float maxVolume = 1.0f;
 
+	[Space(5)]
+	[Tooltip("Below this speed (m/s) volume and pitch are faded out to zero. Above this speed pitch is interpolated to maxPitch at maxSpeed")]
+	public float minSpeed = 50 / 3.6f;  // 50 km/h
+	[Tooltip("Pitch is interpolated between minSpeed and maxSpeed (m/s) from minPitch to maxPitch")]
+	public float maxSpeed = 100.0f; 	// 360 km/h
+	[Tooltip("Minimum audio pitch at minSpeed")]
+	public float minPitch = 0.9f;
+	[Tooltip("Maximum audio pitch at maxSpeed and above")]
+	public float maxPitch = 1.1f;
+
 	[Header("3D Audio Source")]
 	[Tooltip("Maximum volume under this distance")]
 	public float minDistance = 2.0f;
@@ -235,13 +245,19 @@ public class Perrinn424Underfloor : VehicleBehaviour
 			return;
 
 		AudioSource source = audio.source;
+		float absSpeed = MathUtility.FastAbs(vehicle.speed);
 
-		if (cpData.contactCount > cpData.lastPlayedContact)
+		if (cpData.contactCount > cpData.lastPlayedContact && absSpeed > 0.005f)
 			{
 			if (!source.isPlaying) source.Play();
 
+			float attenuation = Mathf.Clamp01(absSpeed / minSpeed);
 			float intensity = Mathf.Clamp01(cpData.maxContactDepth / cpData.contactPoint.limitContactDepth);
-			source.volume = Mathf.Lerp(minVolume, maxVolume, intensity);
+			source.volume = Mathf.Lerp(minVolume, maxVolume, intensity) * attenuation;
+
+			float speedFactor = Mathf.InverseLerp(minSpeed, maxSpeed, absSpeed);
+			source.pitch = Mathf.Lerp(minPitch, maxPitch, speedFactor) * (0.8f + 0.2f * attenuation);	// Reduce pitch 80%
+			MuteZeroPitch(source);
 
 			cpData.lastPlayedContact = cpData.contactCount;
 			cpData.maxContactDepth = 0.0f;
@@ -296,6 +312,19 @@ public class Perrinn424Underfloor : VehicleBehaviour
 		source.loop = true;
 		source.velocityUpdateMode = AudioVelocityUpdateMode.Dynamic;
 		audio.SetVolumeRolloff(minDistance, 1.0f, attenuationDistance, attenuatedVolume, maxDistance);
+		}
+
+
+	void MuteZeroPitch (AudioSource audio)
+		{
+		// Moving the AudioListener around a source with zero or nearly-zero pitch causes artifacts.
+		// The audio source is muted in such chase to prevent that
+
+		float absPitch = MathUtility.FastAbs(audio.pitch);
+		if (absPitch < 0.05f)
+			{
+			audio.volume *= absPitch / 0.05f;
+			}
 		}
 
 
