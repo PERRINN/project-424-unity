9d02ab116eb85e42220d562e7b8a82f6bd63e281
Manuel Espino <espinolaramanuel@gmail.com>
2021-02-03 17:55:56 +0100

    UserInterfaceController. Toggle UI with space key

---
+++ Assets/Features/UI Design/Scripts/Runtime/UserInterfaceController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Perrinn424.UI
+{
+    public class UserInterfaceController : MonoBehaviour
+    {
+        [SerializeField]
+        private Canvas canvas;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                canvas.enabled = !canvas.enabled;
+            }
+        }
+    } 
+}
