cd83a3cb31e2cb803ae50cf63a17973a10932f3b
Manuel Espino <espinolaramanuel@gmail.com>
2021-02-10 19:25:39 +0100

    User Interface has two modes

--- Assets/Features/UI Design/Scripts/Runtime/UserInterfaceController.cs
+++ Assets/Features/UI Design/Scripts/Runtime/UserInterfaceController.cs
@@ -5,13 +5,26 @@ namespace Perrinn424.UI
     public class UserInterfaceController : MonoBehaviour
     {
         [SerializeField]
-        private Canvas canvas;
+        private Behaviour[] fullUI;
+        [SerializeField]
+        private Behaviour[] fullScreen;
 
+        private bool isFullUI = true;
         private void Update()
         {
             if (Input.GetKeyDown(KeyCode.Space))
             {
-                canvas.enabled = !canvas.enabled;
+                isFullUI = !isFullUI;
+                SetEnable(fullUI, isFullUI);
+                SetEnable(fullScreen, !isFullUI);
+            }
+        }
+
+        private void SetEnable(Behaviour[] componentArray, bool isEnable)
+        {
+            foreach (Behaviour component in componentArray)
+            {
+                component.enabled = isEnable;
             }
         }
     } 
