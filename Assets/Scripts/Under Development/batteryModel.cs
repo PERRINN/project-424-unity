0c010f8564a2892b0eccddac4689ddec9826ff69
Manuel Espino <espinolaramanuel@gmail.com>
2021-02-17 17:58:07 +0100

    BatteryModel cleanup

--- Assets/Scripts/Under Development/batteryModel.cs
+++ Assets/Scripts/Under Development/batteryModel.cs
@@ -1,24 +1,8 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using VehiclePhysics;
+﻿using VehiclePhysics;
 using VehiclePhysics.UI;
 
-public class batteryModel : MonoBehaviour
+public class batteryModel : VehicleBehaviour
 {
-    public VehicleBase vehicle;
-
-    // public Vector2 position = new Vector2(8, 8);
-    // public Font font;
-    // [Range(6, 100)]
-    // public int fontSize = 17;
-    // public Color fontColor = Color.white;
-    //
-    string m_text = "";
-    GUIStyle m_textStyle = new GUIStyle();
-    float m_boxWidth;
-    float m_boxHeight;
-
     public static float batteryCapacity;
     float batteryTemprature;
     public static float batterySOC;
@@ -30,23 +14,17 @@ public class batteryModel : MonoBehaviour
 
 
     public static float powerTotal;
-    //float elapsed = 0f;
-
 
     private void Start()
     {
         batteryCapacity = 55;
         batterySOC = batteryCapacity;
         batteryDOD = 0;
-
-        // m_textStyle.font = font;
-        // m_textStyle.fontSize = fontSize;
-        // m_textStyle.normal.textColor = fontColor;
     }
 
-    private void FixedUpdate()
-    {
 
+    public override void FixedUpdateVehicle()
+    {
         int[] custom = vehicle.data.Get(Channel.Custom);
 
         frontPower = custom[Perrinn424Data.FrontMguBase + Perrinn424Data.ElectricalPower] / 1000.0f;
@@ -54,30 +32,14 @@ public class batteryModel : MonoBehaviour
 
         powerTotal = frontPower + rearPower;
 
-
-
-
-        //elapsed += Time.deltaTime;
-        //if (elapsed >= 1f)
-        //{
-        //    elapsed = elapsed % 1f;
-        //    batteryCharge();
-        //}
-
         batteryCharge();
-
 
         batterySOC = (batteryCapacity / 55) * 100;
         batteryDOD = 100 - batterySOC;
+    }
 
-        //print("State of charge: " + batterySOC + "%");
-        //print("Depth of discharge: " + batteryDOD + "%");
-
-        //print(batteryCapacity);
-        //print(powerTotal);
-
-
-
+    public override void UpdateVehicle()
+    {
         SteeringScreen.batSOC = batterySOC;
         SteeringScreen.batCapacity = batteryCapacity;
     }
@@ -98,31 +60,4 @@ public class batteryModel : MonoBehaviour
             batteryCapacity = 0;
         }
     }
-
-    void OnGUI()
-    {
-        // Compute box size
-
-        //m_text = "Battery SOC (%): " + System.Math.Round(batterySOC) + "\n";
-        //m_text += "Power used (kW): " + System.Math.Round(powerTotal) + "\n";
-        //m_text += "Energy left (kWh): " + System.Math.Round(batteryCapacity) + "\n";
-
-        //Vector2 contentSize = m_textStyle.CalcSize(new GUIContent(m_text));
-        //float margin = m_textStyle.lineHeight * 1.2f;
-        //float headerHeight = GUI.skin.box.lineHeight;
-
-        //m_boxWidth = contentSize.x + margin;
-        //m_boxHeight = contentSize.y + headerHeight + margin / 2;
-
-        //// Compute box position
-
-        //float xPos = position.x < 0 ? Screen.width + position.x - m_boxWidth : position.x;
-        //float yPos = position.y < 0 ? Screen.height + position.y - m_boxHeight : position.y;
-
-        //// Draw telemetry box
-
-        //GUI.Box(new Rect(xPos, yPos, m_boxWidth, m_boxHeight), "424 Battery Model");
-        //GUI.Label(new Rect(xPos + margin / 2, yPos + margin / 2 + headerHeight, Screen.width, Screen.height), m_text, m_textStyle);
-    }
-
 }
