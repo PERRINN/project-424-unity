692216d094e5bb43a2f1712475263925b244cd62
Edy <edytado@gmail.com>
2021-02-26 14:40:00 +0100

    Disabled LapTimerListener component
    It raises a nullptr error when receiving the onLap event, causing the basic LapTimer component to fail as well.

--- Assets/Vehicle Physics Pro/Sample Assets/Scripts/Timing/LapTimer.cs
+++ Assets/Vehicle Physics Pro/Sample Assets/Scripts/Timing/LapTimer.cs
@@ -238,7 +238,7 @@ public class LapTimer : MonoBehaviour
 					{
 					m_sectors[sectors-1] = hitTime - m_sectorStartTime;
 					m_validSectors[sectors-1] = !m_invalidSector;
-					// if (onLap != null) onLap(lapTime, !m_invalidLap, m_sectors, m_validSectors);
+					if (onLap != null) onLap(lapTime, !m_invalidLap, m_sectors, m_validSectors);
 
 					if (debugLog)
 						{
