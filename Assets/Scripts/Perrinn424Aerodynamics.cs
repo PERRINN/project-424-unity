cad84b785ddc514dec0e401a33b2bd7ef3aa6fb5
Augusto Dufloth <dufloth@gmail.com>
2021-03-01 14:58:48 -0300

    Calculating air density from altitude position. Note that the altitude currently is a fixed placeholder and should be replaced with the car height above sea level.

--- Assets/Scripts/Perrinn424Aerodynamics.cs
+++ Assets/Scripts/Perrinn424Aerodynamics.cs
@@ -2,8 +2,9 @@ using UnityEngine;
 using VehiclePhysics;
 using System;
 
-// THINGS TO MODIFY:
-// bodyRoll: Change to the correct vehicle body roll due to suspension kinematics
+
+
+
 public class Perrinn424Aerodynamics : VehicleBehaviour
 {
 	[Serializable]
@@ -25,7 +26,8 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 		public float frontFlapCoefficient        = 1.0f;
 	}
 
-	public float airDensity         = 1.0f;
+	public float positionAltitude   = 1.0f;
+	public float deltaISA           = 1.0f;
 
 	public float flapAngle          = 1.0f;
 	public float dRSActivationDelay = 1.0f;
@@ -36,7 +38,7 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 	public AeroSettings rear  = new AeroSettings();
 	public AeroSettings drag  = new AeroSettings();
 
-	[HideInInspector] public bool DRSclosing  = false;
+	[HideInInspector] public bool  DRSclosing  = false;
 	[HideInInspector] public float DRS = 0;
 	[HideInInspector] public float SCzFront = 0;
 	[HideInInspector] public float SCzRear  = 0;
@@ -54,19 +56,77 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 	float DRStime = 0;
 	bool DRSStatus = false;
 
-	//%  Function Name: CalcAeroCoeff 
-	//%  This function calculates a given aerodynamic coefficient based on:
-	//% 
-	//%	 [IN]	aeroSetting [AeroSettings]
-	//%	 [IN]	fRH_mm [mm]
-	//%	 [IN]	rRH_mm [mm]
-	//%	 [IN]	yawAngle_deg [deg]
-	//%	 [IN]	steerAngle_deg [deg]
-	//%	 [IN]	rollAngle_deg [deg]
-	//%	 [IN]	DRS [-]
-	//%	 [IN]	flapAngle_deg [deg]
-	//%
-	//%	 [OUT]	SCn [m2]
+
+	// Constants for the atmosphere model
+	private const double tHeight = 11000;			// Tropopause Height [m]
+	private const double e = 4.25587981;			// Gas Constant Ratio
+	private const double rho0 = 1.225;              // Sea Level air density at ISA [m/s]
+	private const double vs0 = 340.294005808213;	// Sea Level speed of sound [m/s]
+	private const double mu0 = 1.7894E-005;			// Sea Level air viscosity
+
+	// Function name: CalcRho
+	// This function calculates the air density at a specific geometric altitude and delta ISA temperature.
+	// Many calculations are commented because we don't need them from the atmosphere model
+	//
+	//   [IN]	alt		[m]
+	//   [IN]   dISA	[degC]
+	//		
+	//	 [OUT]	rho		[kg/m3]
+	double CalcRho(float alt, float dISA)
+    {
+		double T_std    = 0;
+		//double P_P0     = 0;
+		double Rho_Rho0 = 0;
+		//double Nhp      = 0;
+		double Tisa     = dISA;
+		// double T_kelvin = 0;
+		// double T_T0 = 0;
+		double H = alt / tHeight;
+		// double DhpDh = 0;
+		// double Vsound = 0;
+		double Rho_Std = 0;
+		double Rho = 0;
+		// double Sqrt_Sigma = 0;
+		// double mu = 0;
+		
+		//if (H < 1)
+        //{
+		T_std = 288.15 - 71.5 * H;
+		//P_P0 = Math.Pow(1.0 - 0.248134652090925 * H, e + 1);
+		Rho_Rho0 = Math.Pow(1.0 - 0.248134652090925 * H, e);
+		//} 
+		//else  // I'm assuming we'll never go this route ;)
+        //{
+		//	T_std = 216.65;
+		//	Nhp = Math.Exp(-1.73457 * H);
+		//	P_P0 = Nhp * 1.26567;
+		//	Rho_Rho0 = Nhp * 1.68338;
+		//}
+		//T_kelvin = Tisa + T_std;
+		//T_T0 = T_kelvin / 288.15;
+		// DhpDh = T_std / (T_std + Tisa);
+		// Vsound = Math.Sqrt(T_T0) * vs0;
+		Rho_Std = Rho_Rho0 * rho0;
+		Rho = Rho_Std / (1 + Tisa / T_std);
+		// Sqrt_Sigma = Math.Sqrt(Rho / rho0);
+		// mu = mu0 * (Math.Pow(T_kelvin, 1.5) / (T_kelvin + 120) / 11.984);
+
+		return Rho;
+    }
+
+	// Function Name: CalcAeroCoeff 
+	// This function calculates a given aerodynamic coefficient based on:
+	// 
+	//	 [IN]	aeroSetting [AeroSettings]
+	//	 [IN]	fRH_mm [mm]
+	//	 [IN]	rRH_mm [mm]
+	//	 [IN]	yawAngle_deg [deg]
+	//	 [IN]	steerAngle_deg [deg]
+	//	 [IN]	rollAngle_deg [deg]
+	//	 [IN]	DRS [-]
+	//	 [IN]	flapAngle_deg [deg]
+	//
+	//	 [OUT]	SCn [m2]
 	float CalcAeroCoeff(AeroSettings aeroSetting, float fRH_mm, float rRH_mm, float yawAngle_deg, float steerAngle_deg, float rollAngle_deg, float DRSpos, float flapAngle_deg)
 	{
 		// Assigning return variable
@@ -95,33 +155,16 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 		return SCn;
 	}
 
-	//%  Function Name: ConvertAngle 
-	//%  This function converts the an angle from 0/360 to -180/180 deg.
-	//% 
-	//%	 [IN]	angle_deg [deg]
-	//%
-	//%	 [OUT]	angle_deg [deg]
-	//%********************************************************************
-	float ConvertAngle(float angle_deg)
-    {
-		if (angle_deg > 180)
-			angle_deg -= 360;
 
-		return angle_deg;
-	}
-
-
-	//%********************************************************************
-	//%  Function Name: CalcDRSPosition 
-	//%  This function calculates the DRS position
-	//% 
-	//%	 [IN]	throttlePos: 0 to 1 [-]
-	//%	 [IN]	brakePos: 0 to 1 [-]
-	//%	 [IN]	DRSpos: 0 to 1 [-]
-	//%
-	//%	 [OUT]	DRSpos: 0 to 1 [-]
-	//%********************************************************************
-	float CalcDRSPosition(float throttlePos, float brakePos, float DRSpos)
+	//  Function Name: CalcDRSPosition 
+	//  This function calculates the DRS position
+	// 
+	//	 [IN]	throttlePos: 0 to 1 [-]
+	//	 [IN]	brakePos:    0 to 1 [-]
+	//	 [IN]	DRSpos:      0 to 1 [-]
+	//
+	//	 [OUT]	DRSpos:      0 to 1 [-]
+		float CalcDRSPosition(float throttlePos, float brakePos, float DRSpos)
 	{
 		if (throttlePos == 1 && brakePos == 0 && !DRSclosing)
 		{
@@ -148,25 +191,25 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 
 	public override void FixedUpdateVehicle()
 	{
-		// Assisgning local variables
 		Rigidbody rb = vehicle.cachedRigidbody;
-		float vSquared        = rb.velocity.sqrMagnitude;
-		float dynamicPressure = (float)(airDensity * vSquared / 2.0);
-		
-		yawAngle   = Math.Min(Math.Max(Math.Abs(vehicle.speedAngle), 0), 10);
-		steerAngle = Math.Min(Math.Max(Math.Abs(Math.Abs(vehicle.wheelState[0].steerAngle + vehicle.wheelState[1].steerAngle) / 2), 0), 20);
-
-		fronRollAngle = vehicle.data.Get(Channel.Custom, Perrinn424Data.FrontRollAngle) / 1000.0f;
-		rearRollAngle = vehicle.data.Get(Channel.Custom, Perrinn424Data.RearRollAngle) / 1000.0f;
-		rollAngle = Math.Abs((fronRollAngle + rearRollAngle) / 2);
-
-		frontRideHeight = vehicle.data.Get(Channel.Custom, Perrinn424Data.FrontRideHeight);
-		rearRideHeight  = vehicle.data.Get(Channel.Custom, Perrinn424Data.RearRideHeight);
 
 		// Getting driver's input
-		int[] input = vehicle.data.Get(Channel.Input);
+		int[] input            = vehicle.data.Get(Channel.Input);
 		float throttlePosition = input[InputData.Throttle] / 10000.0f;
-		float brakePosition = input[InputData.Brake] / 10000.0f;
+		float brakePosition    = input[InputData.Brake] / 10000.0f;
+
+		// Calculating dynamic pressure
+		float vSquared = rb.velocity.sqrMagnitude;
+		float dynamicPressure = (float)(CalcRho(positionAltitude, deltaISA) * vSquared / 2.0);
+
+		// Setting vehicle parameters for the aero model
+		yawAngle        = vehicle.speedAngle;
+		steerAngle      = (vehicle.wheelState[0].steerAngle + vehicle.wheelState[1].steerAngle) / 2;
+		fronRollAngle   = vehicle.data.Get(Channel.Custom, Perrinn424Data.FrontRollAngle) / 1000.0f;
+		rearRollAngle   = vehicle.data.Get(Channel.Custom, Perrinn424Data.RearRollAngle) / 1000.0f;
+		rollAngle       = (fronRollAngle + rearRollAngle) / 2;
+		frontRideHeight = vehicle.data.Get(Channel.Custom, Perrinn424Data.FrontRideHeight);
+		rearRideHeight  = vehicle.data.Get(Channel.Custom, Perrinn424Data.RearRideHeight);
 
 		// Calculating DRS position and feeding to the car data bus
 		DRS = CalcDRSPosition(throttlePosition, brakePosition, DRS);
@@ -197,7 +240,6 @@ public class Perrinn424Aerodynamics : VehicleBehaviour
 		downforceFront = SCzFront * dynamicPressure;
 		downforceRear  = SCzRear  * dynamicPressure;
 		dragForce      = SCx      * dynamicPressure;
-
 	}
 
 }
