d3b0a855568cecc0ec1465940feee6626021371f
Manuel Espino <espinolaramanuel@gmail.com>
2021-03-14 12:19:21 +0100

    FixedCamerasController

---
+++ Assets/Features/Camera/Fixed Cameras Controller/FixedCamerasController.cs
@@ -0,0 +1,38 @@
+﻿using Perrinn424.Utils;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perrinn424.CameraSystem
+{
+    internal class FixedCamerasController : MonoBehaviour
+    {
+        private CircularBuffer<Camera> circularBuffer;
+
+        private void Awake()
+        {
+            Camera[] cameras = this.GetComponentsInChildren<Camera>(true);
+
+            foreach (Camera c in cameras)
+            {
+                c.gameObject.SetActive(false);
+            }
+
+            circularBuffer = new CircularBuffer<Camera>(cameras);
+
+            circularBuffer.Current.gameObject.SetActive(true);
+        }
+
+        public void NextCamera()
+        {
+            circularBuffer.Current.gameObject.SetActive(false);
+            circularBuffer.MoveNext().gameObject.SetActive(true);
+        }
+
+        public void PreviousCamera()
+        {
+            circularBuffer.Current.gameObject.SetActive(false);
+            circularBuffer.MovePrevious().gameObject.SetActive(true);
+        }
+    } 
+}
---
+++ Assets/Features/Camera/Fixed Cameras Controller/FixedCamerasControllerInput.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perrinn424.CameraSystem
+{
+    [RequireComponent(typeof(FixedCamerasController))]
+    public class FixedCamerasControllerInput : MonoBehaviour
+    {
+        [SerializeField]
+        private FixedCamerasController fixedCamerasController;
+        
+        [SerializeField]
+        private KeyCode nextCameraKey = KeyCode.P;
+        
+        [SerializeField]
+        private KeyCode previousCameraKey = KeyCode.O;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(nextCameraKey))
+            {
+                fixedCamerasController.NextCamera();
+            }
+            else if (Input.GetKeyDown(nextCameraKey))
+            {
+                fixedCamerasController.PreviousCamera();
+            }
+        }
+
+        private void Reset()
+        {
+            fixedCamerasController = this.GetComponent<FixedCamerasController>();
+        }
+    } 
+}
