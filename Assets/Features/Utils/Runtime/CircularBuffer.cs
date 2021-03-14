33a7ed8e4e68a52be5a65e3906ce4703de0f60ec
Manuel Espino <espinolaramanuel@gmail.com>
2021-03-14 12:01:46 +0100

    CircularBuffer

---
+++ Assets/Features/Utils/Runtime/CircularBuffer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Perrinn424.Utils
+{
+    public class CircularBuffer<T>
+    {
+        private T[] array;
+        private int index;
+        private int length;
+        public T Current 
+        {
+            get => array[index];
+        }
+        public CircularBuffer(IEnumerable<T> elements)
+        {
+            index = 0;
+            this.array = elements.ToArray();
+            length = array.Length;
+        }
+
+        public T MoveNext()
+        {
+            IncreaseIndex(1);
+            return Current;
+        }
+
+        public T MovePrevious()
+        {
+            IncreaseIndex(-1);
+            return Current;
+        }
+
+
+        public void Reset()
+        {
+            index = 0;
+        }
+
+        private void IncreaseIndex(int delta)
+        {
+            index = index + delta;
+
+            if (index > length - 1)
+            {
+                index = index % length;
+            }
+            else if (index < 0)
+            {
+                index = length + index; //index here is negative
+            }
+        }
+    } 
+}
---
+++ Assets/Features/Utils/Tests/UtilsTests.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Perrinn424.Utils.Editor.Tests
+{
+    public class UtilsTests
+    {
+        // A Test behaves as an ordinary method
+        [Test]
+        public void CircularBufferTest()
+        {
+            int[] array = { 0, 1, 2, 3, 4 };
+            CircularBuffer<int> buffer = new CircularBuffer<int>(array);
+
+            void Next()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    buffer.MoveNext();
+                }
+            }
+
+            void Previous()
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    buffer.MovePrevious();
+                }
+            }
+
+            Assert.DoesNotThrow(Next);
+            Assert.DoesNotThrow(Previous);
+
+            buffer.Reset();
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int previous = array[i-1];
+                int current = array[i];
+                Assert.AreEqual(previous, buffer.Current);
+                Assert.AreEqual(current, buffer.MoveNext());
+                Assert.AreEqual(current, buffer.Current);
+            }
+
+            //Circular to the right
+            Assert.AreEqual(array[0], buffer.MoveNext());
+
+
+
+            buffer.Reset();
+            //Circular to the left
+            buffer.MovePrevious();
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                int current = array[i];
+                Assert.AreEqual(current, buffer.Current);
+                buffer.MovePrevious();
+            }
+        }
+    }
+}
