0b5a78dc9ab8ac1c0eb3476dd2891d297ed0b840
Manuel Espino <espinolaramanuel@gmail.com>
2021-03-15 18:42:55 +0100

    Tests fixed

--- Assets/Features/UI Design/Scripts/Tests/Editor/TimeReferenceTests.cs
+++ Assets/Features/UI Design/Scripts/Tests/Editor/TimeReferenceTests.cs
@@ -6,9 +6,9 @@ namespace Perrinn424.Editor.Tests
 {
     public class TimeReferenceTests
     {
-        [TestCase(13.41127f, 657.8914f, 0.4128628f)]
-        [TestCase(24.60585f, 1391.423f, -1.513094f)]
-        [TestCase(5.562164f, 544.5441f, -5.779054f)]
+        [TestCase(13.41127f, 657.8914f, 0.370546341f)]
+        [TestCase(24.60585f, 1391.423f, -1.62420464f)]
+        [TestCase(5.562164f, 544.5441f, -5.81348467f)]
         public void TimeReferenceTest(float time, float distance, float expectedDifference)
         {
             TimeReference porsche = TimeReferenceHelper.CreatePorsche();
