6b062e7b493305da9959c5369126ada5f8463dc8
Edy <edytado@gmail.com>
2021-05-13 19:36:14 +0200

    Telemetry emitter for the generic 424 channels

---
+++ Assets/Scripts/Telemetry/Perrinn424GenericTelemetry.cs
@@ -0,0 +1,85 @@
+
+using UnityEngine;
+using VehiclePhysics;
+
+
+public class Perrinn424GenericTelemetry : VehicleBehaviour
+	{
+	public bool emitTelemetry = true;
+
+
+	public override bool EmitTelemetry ()
+		{
+		return emitTelemetry;
+		}
+
+
+	public override void RegisterTelemetry ()
+		{
+		vehicle.telemetry.Register<Perrinn424Inputs>(vehicle);
+		}
+
+
+	public override void UnregisterTelemetry ()
+		{
+		vehicle.telemetry.Unregister<Perrinn424Inputs>(vehicle);
+		}
+
+
+
+	public class Perrinn424Inputs : Telemetry.ChannelGroup
+		{
+		Steering.Settings m_steering;
+		Perrinn424CarController m_controller;
+
+
+		public override int GetChannelCount ()
+			{
+			return 4;
+			}
+
+
+		public override float GetPollFrequency ()
+			{
+			return 50.0f;
+			}
+
+
+		public override void GetChannelInfo (Telemetry.ChannelInfo[] channelInfo, Object instance)
+			{
+			// Fill-in the channel information
+
+			// channelInfo[0].SetNameAndSemantic("SteeringAngle", Telemetry.Semantic.SteerAngle);
+			// channelInfo[0].SetNameAndSemantic("BrakePressure", Telemetry.Semantic.BrakePressure);
+			channelInfo[0].SetNameAndSemantic("Gear", Telemetry.Semantic.Gear);
+			channelInfo[1].SetNameAndSemantic("SteeringAngle", Telemetry.Semantic.Default);
+			channelInfo[2].SetNameAndSemantic("Throttle", Telemetry.Semantic.Ratio);
+			channelInfo[3].SetNameAndSemantic("BrakePressure", Telemetry.Semantic.Default);
+
+			// Access to information in the vehicle
+
+			VehicleBase vehicle = instance as VehicleBase;
+			m_steering = vehicle.GetInternalObject(typeof(Steering.Settings)) as Steering.Settings;
+			m_controller = vehicle.GetComponent<Perrinn424CarController>();
+			}
+
+
+		public override void PollValues (float[] values, int index, Object instance)
+			{
+			// Gear
+
+			values[index+0] = m_controller.gear;
+
+			// Steer angle
+
+			float steerInput = m_controller.data.Get(Channel.Vehicle, VehicleData.AidedSteer) / 10000.0f;
+			float steerAngle = steerInput * m_steering.steeringWheelRange * 0.5f;
+			values[index+1] = steerAngle;
+
+			// Throttle and brake
+
+			values[index+2] = m_controller.throttleInput;
+			values[index+3] = m_controller.brakePressure;
+			}
+		}
+	}
