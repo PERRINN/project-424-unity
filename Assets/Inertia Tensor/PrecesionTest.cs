60c2570be6a2eb81290e26c70753a548b4e651c0
Manuel Espino <espinolaramanuel@gmail.com>
2021-10-07 17:32:28 +0200

    Add Precession Scripts

---
+++ Assets/Inertia Tensor/PrecesionTest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class PrecesionTest : MonoBehaviour
+{
+    [SerializeField]
+    private Rigidbody rb;
+    private PrecessionEffect preccesion;
+    
+    private void OnEnable()
+    {
+        preccesion = new PrecessionEffect(rb);
+    }
+
+    private void FixedUpdate()
+    {
+        preccesion.Apply();
+    }
+}
---
+++ Assets/Inertia Tensor/PrecessionEffect.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class PrecessionEffect
+{
+    public Rigidbody rb;
+    public Matrix4x4 InertiaTensor { get; private set; }
+    public Vector3 AngularVelocity { get; private set; }
+    public Vector3 AngularMomentum { get; private set; }
+    public Vector3 AngularMomentum_derivative { get; private set; }
+    public Vector3 AngularAcceleration { get; private set; }
+
+    public PrecessionEffect(Rigidbody rb)
+    {
+        this.rb = rb;
+        InertiaTensor = InertiaTensorUtils.CalculateInertiaTensorMatrix(rb);
+    }
+
+    public void Apply()
+    {
+        AngularVelocity = rb.angularVelocity;
+        AngularMomentum = InertiaTensor * AngularVelocity;
+        AngularMomentum_derivative = Vector3.Cross(AngularVelocity, AngularMomentum);
+        AngularAcceleration = InertiaTensor.inverse * AngularMomentum_derivative;
+        rb.AddTorque(AngularAcceleration, ForceMode.Acceleration);
+    }
+}
