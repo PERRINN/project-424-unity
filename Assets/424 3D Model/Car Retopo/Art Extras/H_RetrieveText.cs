2a955f3a65c31721c8c12385ce741c17ada1e136
HelioZuazua666 <heliozuazua@gmail.com>
2021-11-26 11:13:09 +0100

    ART 1º REVIEW 11/21
    ART 1º REVIEW : Rotations , Materials and minor changes

---
+++ Assets/424 3D Model/Car Retopo/Art Extras/H_RetrieveText.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class H_RetrieveText : MonoBehaviour
+{
+
+    public string members;
+    public Text membersText;
+    void Start()
+    {
+        StartCoroutine(GetText());
+    }
+
+    IEnumerator GetText()
+    {
+        UnityWebRequest www = UnityWebRequest.Get("https://perrinn.com/directory");
+        yield return www.SendWebRequest();
+
+        
+        
+            // Show results as text
+            Debug.Log(www.downloadHandler.text);
+
+            // Or retrieve results as binary data
+            byte[] results = www.downloadHandler.data;
+
+
+        //print data into
+        members = www.downloadHandler.text;
+        membersText.text = members;
+
+
+    }
+}
