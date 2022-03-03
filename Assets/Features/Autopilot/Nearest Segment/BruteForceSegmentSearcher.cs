39588f940c0ebce493d5208898a2de0336388552
Manuel Espino <espinolaramanuel@gmail.com>
2022-03-03 18:04:00 +0100

    Add INearestSegmentSearcher

---
+++ Assets/Features/Autopilot/Nearest Segment/BruteForceSegmentSearcher.cs
@@ -0,0 +1,45 @@
+﻿using Perrinn424.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BruteForceSegmentSearcher : INearestSegmentSearcher
+{
+
+    private readonly IReadOnlyList<Vector3> path;
+    public BruteForceSegmentSearcher(IReadOnlyList<Vector3> path)
+    {
+        this.path = path;
+    }
+    public int StartIndex { get; private set; }
+
+    public int EndIndex { get; private set; }
+
+    public void Search(Transform t)
+    {
+        float sqrtDistance = Mathf.Infinity;
+
+        CircularIndex closestIndex = new CircularIndex(path.Count);
+        for (int i = 0; i < path.Count; i++)
+        {
+            float tempDistance = (t.position - path[i]).sqrMagnitude;
+            if (tempDistance < sqrtDistance)
+            {
+                sqrtDistance = tempDistance;
+                closestIndex.Assign(i);
+            }
+        }
+
+        Vector3 segmentVector = path[closestIndex + 1] - path[closestIndex];
+        Vector3 pointVector = t.position - path[closestIndex];
+
+        if(Vector3.Dot(segmentVector, pointVector) < 0)
+        {
+            closestIndex--;
+        }
+
+        StartIndex = closestIndex;
+        EndIndex = closestIndex + 1;
+    }
+
+}
---
+++ Assets/Features/Autopilot/Nearest Segment/INearestSegmentSearcher.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public interface INearestSegmentSearcher
+{
+    void Search(Transform t);
+
+    int StartIndex { get;}
+    int EndIndex { get; }
+}
