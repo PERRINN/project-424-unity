eddc00d968930cfad19511d6e90231802e4df0e0
Manuel Espino <espinolaramanuel@gmail.com>
2022-04-02 09:21:56 +0200

    Clean

--- Assets/Features/Autopilot/Nearest Neighbor/SectorSearcherNearestNeighbor.cs
+++ Assets/Features/Autopilot/Nearest Neighbor/SectorSearcherNearestNeighbor.cs
@@ -30,19 +30,6 @@ namespace Perrinn424.AutopilotSystem
             Index = index;
             Distance = distance;
             Position = path[Index];
-
-            //(int nnSector, int searchedSectorSize) = SearchNearestSector(position, sectorSize, tolerance, 0);
-
-            //int securityMargin = 3;
-            //int from = nnSector - Mathf.CeilToInt(searchedSectorSize / 2f) - securityMargin * searchedSectorSize;
-            //int count = sectorSize + 2 * securityMargin * searchedSectorSize; //searchSize/2 to the left, searchSize/2 to the right, and +1 because of the middle point
-            //int step = 1;
-
-            //(int index, float distance) = bruteForceSearcher.SearchInBoundaries(position, from, count, step);
-
-            //Index = index;
-            //Distance = distance;
-            //Position = path[Index];
         }
 
         public (int, float) SearchRecursive(Vector3 position, int searchSize, float tolerance, int depth)
@@ -67,18 +54,6 @@ namespace Perrinn424.AutopilotSystem
 
             return (index, distance); //base case
         }
-
-        //private (int, int) SearchNearestSector(Vector3 position, int searchSize, float tolerance, int depth)
-        //{
-        //    (int nearestSector, float distance) = bruteForceSearcher.SearchInBoundaries(position, 0, path.Count, searchSize);
-
-        //    if (distance > tolerance && depth <= maxDepth)
-        //    {
-        //        return SearchNearestSector(position, (searchSize / 2) + 1, tolerance, depth + 1);
-        //    }
-
-        //    return (nearestSector, searchSize);
-        //}
 
         public int Index { get; private set; }
 
