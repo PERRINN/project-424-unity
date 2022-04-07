1f7ac0d49581bbfbd181989e0a1c14b48c4fd90f
Manuel Espino <espinolaramanuel@gmail.com>
2022-04-07 18:25:50 +0200

    Add SteeringWheelVisibility

---
+++ Assets/SteeringWheelVisibilityController.cs
@@ -0,0 +1,51 @@
+﻿using Perrinn424.UI;
+using Perrinn424.Utilities;
+using UnityEngine;
+
+public class SteeringWheelVisibilityController : MonoBehaviour
+{
+    public KeyCode nextOptionKey;
+    public Dashboard dashboard;
+    public GameObject steeringWheel;
+    public CircularIndex options;
+
+    private void OnEnable()
+    {
+        options = new CircularIndex(0, 3);
+        SteeringWheelVisibility();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(nextOptionKey))
+        {
+            options++;
+            SteeringWheelVisibility();
+        }
+    }
+
+    private void SteeringWheelVisibility()
+    {
+        switch (options)
+        {
+            case 0:
+                SteeringWheelVisibility(true, false);
+                break;
+            case 1:
+                SteeringWheelVisibility(false, true);
+                break;
+            case 2:
+                SteeringWheelVisibility(false, false);
+                break;
+        }
+
+    }
+
+    private void SteeringWheelVisibility(bool showSteeringWheel, bool showDashboard)
+    {
+        steeringWheel.SetActive(showSteeringWheel);
+        dashboard.gameObject.SetActive(showDashboard);
+    }
+
+
+}
