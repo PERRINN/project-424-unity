c71f97e5635069bd08e9d1b3429721ac93afd55d
Manuel Espino <espinolaramanuel@gmail.com>
2022-04-12 12:31:03 +0200

    Add driver height controller

---
+++ Assets/Features/Utilities/Runtime/DriverHeightController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Perrinn424.Utilities
+{
+    public class DriverHeightController : MonoBehaviour
+    {
+        [SerializeField]
+        private KeyCode increaseHeight;
+        [SerializeField]
+        private KeyCode decreaseHeight;
+
+        [SerializeField]
+        private Transform firstPersonCamera;
+
+        public float step = 0.025f;
+        public float max = 0.05f;
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(increaseHeight))
+            {
+                Change(step);
+            }
+            else if (Input.GetKeyDown(decreaseHeight))
+            {
+                Change(-step);
+            }
+        }
+
+        private void Change(float delta)
+        {
+            Vector3 pos = firstPersonCamera.localPosition;
+            pos.y = Mathf.Clamp(pos.y + delta, -max, max);
+            firstPersonCamera.localPosition = pos;
+        }
+    } 
+}
