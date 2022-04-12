38e501667e46f8051b980b1b8d81cdb5cd499350
Manuel Espino <espinolaramanuel@gmail.com>
2022-04-12 12:45:52 +0200

    Add fov controllers

---
+++ Assets/Features/Utilities/Runtime/DriverCameraAdjustmentsController.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using VehiclePhysics;
+
+namespace Perrinn424.Utilities
+{
+    public class DriverCameraAdjustmentsController : MonoBehaviour
+    {
+
+        [Header("Height Adjustment")]
+        [SerializeField]
+        private KeyCode increaseHeight;
+        [SerializeField]
+        private KeyCode decreaseHeight;
+
+        [SerializeField]
+        private Transform firstPersonCameraTarget;
+
+        public float heightStep = 0.025f;
+        public float heightMax = 0.05f;
+
+        [Header("FOV Adjustment")]
+        [SerializeField]
+        private KeyCode increaseFOV;
+        [SerializeField]
+        private KeyCode decreaseFOV;
+
+        [SerializeField]
+        private VPCameraController firstPersonCamera;
+
+        public float fovStep = 5f;
+        public float fovMin = 25f;
+        public float fovMax = 45f;
+
+
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(increaseHeight))
+            {
+                ChangeDriverHeight(heightStep);
+            }
+            else if (Input.GetKeyDown(decreaseHeight))
+            {
+                ChangeDriverHeight(-heightStep);
+            }
+
+            if (Input.GetKeyDown(increaseFOV))
+            {
+                ChangeCameraFov(fovStep);
+            }
+            else if (Input.GetKeyDown(decreaseFOV))
+            {
+                ChangeCameraFov(-fovStep);
+            }
+        }
+
+        private void ChangeDriverHeight(float delta)
+        {
+            Vector3 pos = firstPersonCameraTarget.localPosition;
+            pos.y = Mathf.Clamp(pos.y + delta, -heightMax, heightMax);
+            firstPersonCameraTarget.localPosition = pos;
+        }
+
+        private void ChangeCameraFov(float delta)
+        {
+            firstPersonCamera.driverCameraFov = Mathf.Clamp(firstPersonCamera.driverCameraFov + delta, fovMin, fovMax);
+        }
+    } 
+}
