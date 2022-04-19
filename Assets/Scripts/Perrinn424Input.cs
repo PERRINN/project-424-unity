afba907c97c83805c2a7255e856b18d199bcde5e
Edy <edytado@gmail.com>
2022-04-19 20:30:56 +0200

    Force Feedback working in the 424

--- Assets/Scripts/Perrinn424Input.cs
+++ Assets/Scripts/Perrinn424Input.cs
@@ -26,11 +26,21 @@ public class Perrinn424Input : VehicleBehaviour
 	{
 	public string inputUserName = "No name";
 
+	[Header("Force Feedback")]
+	public bool forceFeedbackEnabled = true;
+	public bool rumbleEnabled = true;
+
+	[Space(5)]
+	public ForceFeedbackHelper.Settings forceFeedbackSettings = new ForceFeedbackHelper.Settings();
+	public ForceFeedbackHelper.RumbleSettings rumbleSettings = new ForceFeedbackHelper.RumbleSettings();
+
 
 	// Private fields
 
-	bool m_ffEnabled = false;
 	Perrinn424InputUser m_input;
+	ForceFeedbackHelper m_ffHelper;
+	bool m_ffEnabled = false;
+
 	Steering.Settings m_steeringSettings;
 
 
@@ -83,23 +93,13 @@ public class Perrinn424Input : VehicleBehaviour
 
 		// Gear mode
 
-		if (m_input.gearUp.PressedThisFrame())
-			{
-			inputData[InputData.AutomaticGear]++;
-			Debug.Log("Gear Up");
-			}
-
-		if (m_input.gearDown.PressedThisFrame())
-			{
-			inputData[InputData.AutomaticGear]--;
-			Debug.Log("Gear Down");
-			}
+		if (m_input.gearUp.PressedThisFrame()) inputData[InputData.AutomaticGear]++;
+		if (m_input.gearDown.PressedThisFrame()) inputData[InputData.AutomaticGear]--;
 
 		// TODO DRS
 
-		// TODO Process force feedback if available
+		// Process force feedback if available
 
-		/*
 		InputDevice.ForceFeedback forceFeedback = m_input.steer.ForceFeedback();
 
 		if (m_ffEnabled != forceFeedbackEnabled)
@@ -107,18 +107,60 @@ public class Perrinn424Input : VehicleBehaviour
 			if (!forceFeedbackEnabled && forceFeedback != null)
 				forceFeedback.StopAllEffects();
 
+			if (forceFeedbackEnabled)
+				{
+				m_ffHelper = new ForceFeedbackHelper(vehicle);
+				m_ffHelper.settings = forceFeedbackSettings;
+				m_ffHelper.rumbleSettings = rumbleSettings;
+				}
+
 			m_ffEnabled = forceFeedbackEnabled;
 			}
 
 		if (m_ffEnabled && forceFeedback != null)
 			{
+			m_ffHelper.Update();
 			ProcessForceFeedback(forceFeedback);
 			}
-		*/
-
 		}
 
 
+	void ProcessForceFeedback (InputDevice.ForceFeedback forceFeedback)
+		{
+		(float forceFactor, float dragFactor, float frictionFactor) = m_ffHelper.GetForceFeedback();
+
+		// Main force force feedback
+		// The direction of a force is the direction from which it comes.
+		// A positive force on a given axis pushes from the positive toward the negative.
+
+		forceFeedback.force = true;
+		forceFeedback.forceMagnitude = -forceFactor;
+
+		// Play damper resistance based on the weight on the steering wheels
+
+		forceFeedback.damper = true;
+		forceFeedback.damperCoefficient = dragFactor;
+
+		// Play extra friction at low speeds
+
+		forceFeedback.friction = true;
+		forceFeedback.frictionCoefficient = frictionFactor;
+
+		// Apply rumble
+
+		if (rumbleEnabled)
+			{
+			(float rumbleIntensity, float rumbleFrequency) = m_ffHelper.GetRumble();
+
+			forceFeedback.rumble = true;
+			forceFeedback.rumbleMagnitude = rumbleIntensity;
+			forceFeedback.rumbleFrequency = rumbleFrequency;
+			}
+		else
+			{
+			forceFeedback.rumble = false;
+			}
+		}
 	}
 
 }
