1999cc252d8d2c7dbcbb9052195a1459da1a8d92
Edy <edytado@gmail.com>
2022-04-21 19:57:12 +0200

    Force feedback disabled while autopilot is enabled

--- Assets/Features/Autopilot/AutopilotInputController.cs
+++ Assets/Features/Autopilot/AutopilotInputController.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using VehiclePhysics;
+using VehiclePhysics.InputManagement;
+
 
 namespace Perrinn424.AutopilotSystem
 {
@@ -9,64 +11,16 @@ namespace Perrinn424.AutopilotSystem
         [SerializeField]
         private BaseAutopilot autopilot;
 
-        VPDeviceInput m_deviceInput;
-        float m_ffbForceIntensity;
-        float m_ffbDamperCoefficient;
 
-        public override void OnEnableVehicle()
-        {
-            m_deviceInput = vehicle.GetComponentInChildren<VPDeviceInput>();
-            if (m_deviceInput != null)
-            {
-                m_ffbForceIntensity = m_deviceInput.forceIntensity;
-                m_ffbDamperCoefficient = m_deviceInput.damperCoefficient;
-            }
-        }
-
-
-
-        // Update is called once per frame
         void Update()
         {
             if (Input.GetKeyDown(KeyCode.Q))
             {
                 autopilot.ToggleStatus();
-                if (m_deviceInput != null)
-                {
-                    if (autopilot.IsOn)
-                    {
-                        m_deviceInput.forceIntensity = 0.0f;
-                        m_deviceInput.damperCoefficient = 0.0f;
-                    }
-                    else
-                    {
-                        m_deviceInput.forceIntensity = m_ffbForceIntensity;
-                        m_deviceInput.damperCoefficient = m_ffbDamperCoefficient;
-                    }
-                }
-
-                //if (autopilotON)
-                //{
-                //    autopilotON = false;
-                //    SteeringScreen.autopilotState = false;
-                //    if (m_deviceInput != null)
-                //    {
-                //        m_deviceInput.forceIntensity = m_ffbForceIntensity;
-                //        m_deviceInput.damperCoefficient = m_ffbDamperCoefficient;
-                //    }
-                //}
-                //else
-                //{
-                //    autopilotON = true;
-                //    SteeringScreen.autopilotState = true;
-                //    if (m_deviceInput != null)
-                //    {
-                //        m_deviceInput.forceIntensity = 0.0f;
-                //        m_deviceInput.damperCoefficient = 0.0f;
-                //    }
-                //}
+                InputManager.instance.runtime.disableForceFeedback = autopilot.IsOn;
             }
         }
+
 
         private void Reset()
         {
