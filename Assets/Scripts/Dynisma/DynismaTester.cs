209a82e8abc0e5675fb88f86affe1555ce590497
Edy <edytado@gmail.com>
2022-06-05 19:43:41 +0200

    Tester widget showing motion platform data

--- Assets/Scripts/Dynisma/DynismaTester.cs
+++ Assets/Scripts/Dynisma/DynismaTester.cs
@@ -4,6 +4,7 @@
 
 using UnityEngine;
 using EdyCommonTools;
+using System.Text;
 
 
 namespace Perrinn424
@@ -18,16 +19,33 @@ public class DynismaTester : MonoBehaviour
 	public GUITextBox.Settings widget = new GUITextBox.Settings();
 
 
+	struct MotionData
+		{
+		public double accelerationX;			// m/s2
+		public double accelerationY;			// m/s2
+		public double accelerationZ;			// m/s2
+		public double angularAccelerationX;		// rad/s2
+		public double angularAccelerationY;		// rad/s2
+		public double angularAccelerationZ;		// rad/s2
+		public double steeringTorque;			// Nm
+		public double carSpeed;					// m/s
+		public double simulationTime;			// s
+		}
+
+
 	// Trick to configure a default font in the widget. Configure the font at the script settings.
 	[HideInInspector] public Font defaultConsoleFont;
 
-
 	GUITextBox m_widget = new GUITextBox();
+	StringBuilder m_text = new StringBuilder();
+
 	UdpSender m_sender;
 	UdpConnection m_listener = new UdpConnection();
 	UdpListenThread m_thread = new UdpListenThread();
 	byte[] m_buffer = new byte[1024];
 	int m_size = 0;
+	MotionData m_motionData = new MotionData();
+	int m_received = 0;
 
 
 	void OnValidate ()
@@ -45,6 +63,7 @@ public class DynismaTester : MonoBehaviour
 		m_listener.StartConnection(listeningPort);
 		m_thread.Start(m_listener, OnReceiveData);
 		m_size = 0;
+		m_received = 0;
 
 		// Initialize widget
 
@@ -62,6 +81,40 @@ public class DynismaTester : MonoBehaviour
 		}
 
 
+	void Update ()
+		{
+		lock (m_buffer)
+			{
+			if (m_size > 0)
+				{
+				m_motionData = ObjectUtility.GetStructFromBytes<MotionData>(m_buffer);
+				m_size = 0;
+				}
+			}
+
+		UpdateWidgetText();
+		}
+
+
+	void OnGUI ()
+		{
+		m_widget.OnGUI();
+		}
+
+
+	void UpdateWidgetText ()
+		{
+		m_text.Clear();
+		m_text.Append("Motion Platform Data\n\n");
+		m_text.Append($"Packets:               {m_received}\n");
+		m_text.Append($"Acceleration:          X:{m_motionData.accelerationX,12:0.000000}  Y:{m_motionData.accelerationY,12:0.000000}  Z:{m_motionData.accelerationZ,12:0.000000}  m/s2\n");
+		m_text.Append($"Angular Acceleration:  X:{m_motionData.angularAccelerationX,12:0.000000}  Y:{m_motionData.angularAccelerationY,12:0.000000}  Z:{m_motionData.angularAccelerationZ,12:0.000000}  rad/s2\n");
+		m_text.Append($"Steering Torque:      {m_motionData.steeringTorque,11:0.000000}  Nm\n");
+		m_text.Append($"Car Speed:            {m_motionData.carSpeed,11:0.000000}  m/s\n");
+		m_text.Append($"Simulation Time:      {m_motionData.simulationTime,11:0.000000}  s\n");
+		m_widget.text = m_text.ToString();
+		}
+
 	// This is called from the listener thread
 
 	void OnReceiveData ()
@@ -69,10 +122,9 @@ public class DynismaTester : MonoBehaviour
 		lock (m_buffer)
 			{
 			m_size = m_listener.GetMessageBinary(m_buffer);
+			m_received++;
 			}
 		}
-
-
 	}
 
 }
