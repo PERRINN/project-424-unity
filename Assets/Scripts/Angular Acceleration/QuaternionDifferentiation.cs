fd1c213029600904014d5c41065e0d522a5e5ad6
Manuel Espino <espinolaramanuel@gmail.com>
2022-06-07 20:30:24 +0200

    Change Quaternion differentiation to coefficients

--- Assets/Scripts/Angular Acceleration/QuaternionDifferentiation.cs
+++ Assets/Scripts/Angular Acceleration/QuaternionDifferentiation.cs
@@ -10,20 +10,37 @@ public class QuaternionDifferentiation
     public Quaternion rotation;
     public Quaternion rotation_prev;
 
+    public ShiftBuffer<Quaternion> buffer;
+
+    public float[] coefficients;
+
     public void Init(Rigidbody rb)
     {
         this.rb = rb;
 
         rotation = rb.transform.rotation;
         rotation_prev = rotation;
+
+        buffer = new ShiftBuffer<Quaternion>(2);
+        buffer.Fill(rb.transform.rotation);
+
+        coefficients = new[] { 1f, -1f };
     }
 
     public void Compute(float dt)
     {
-        rotation_prev = rotation;
-        rotation = rb.transform.rotation;
+        buffer.Push(rb.transform.rotation);
+
+        Quaternion accumulator = new Quaternion(0f, 0f, 0f, 0f);
+        for (int i = 0; i < buffer.length; i++)
+        {
+            Quaternion summand = ScalarMultiply(buffer[i], coefficients[i]);
+            accumulator = Add(accumulator, summand);
+        }
+
+
         Quaternion diff = Sub(rotation, rotation_prev);
-        Quaternion rotation_dot = ScalarMultiply(diff, 1f / dt);
+        Quaternion rotation_dot = ScalarMultiply(accumulator, 1f / dt);
         w = rotation_dot * Quaternion.Inverse(rotation);
         w = ScalarMultiply(w, 2f);
     }
