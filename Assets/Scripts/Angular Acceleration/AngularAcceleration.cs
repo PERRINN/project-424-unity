606c3f689916187f5b707b4be92572eeff99b846
Manuel Espino <espinolaramanuel@gmail.com>
2022-06-07 21:07:26 +0200

    Use backward coefficients

--- Assets/Scripts/Angular Acceleration/AngularAcceleration.cs
+++ Assets/Scripts/Angular Acceleration/AngularAcceleration.cs
@@ -12,11 +12,25 @@ public class AngularAcceleration : VehicleBehaviour
     {
         rb = vehicle.cachedRigidbody;
         float[] coefficients = new float[0];
+        float divisor = 1f;
+
+        //backward coefficients
         coefficients = new float[] { 1f, -1f };
         //coefficients = new float[] { 1.5f, -2f, 0.5f };
         //coefficients = new float[] { 11/6f, -18/6f, 9/6f, -2/6f };
         //coefficients = new float[] { 25 / 12f, -48 / 12f, 36 / 12f, -16 / 12f, 3/12f };
         //coefficients = new float[] { 147 / 60f, -360 / 60f, 450 / 60f, -400 / 60f, 225/60f, -72/60f, 10/60f };
+
+        //center coefficients
+        //coefficients = new float[] { -1/12f, 8f/12f, 0f, -8/12f, 1f/12f }; //divisor = 1
+        //coefficients = new float[] { 2f, -25, 150, -600, 2100, 0, -2100, 600, -150, 25, -2 }; //divisor = 2520
+        //divisor = 2520f;
+
+        for (int i = 0; i < coefficients.Length; i++)
+        {
+            coefficients[i] /= divisor;
+        }
+
         angularVelocityDifferentiation.Init(rb, coefficients);
 
         quaternionDifferentiation.Init(rb);
