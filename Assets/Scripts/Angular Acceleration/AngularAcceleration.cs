664ea2c8b6817c1823ef92c4f384c572e6176632
Manuel Espino <espinolaramanuel@gmail.com>
2022-06-07 18:53:21 +0200

    Add Angular Acceleration methods

---
+++ Assets/Scripts/Angular Acceleration/AngularAcceleration.cs
@@ -0,0 +1,39 @@
+﻿using Perrinn424.TelemetryLapSystem;
+using UnityEngine;
+using VehiclePhysics;
+
+public class AngularAcceleration : VehicleBehaviour
+{
+    Rigidbody rb;
+    public AngularVelocityDifferentiation angularVelocityDifferentiation;
+
+    public override void OnEnableVehicle()
+    {
+        rb = vehicle.cachedRigidbody;
+        angularVelocityDifferentiation = new AngularVelocityDifferentiation(rb);
+    }
+
+
+    public override void UpdateVehicle()
+    {
+    }
+
+    public override void FixedUpdateVehicle()
+    {
+        angularVelocityDifferentiation.Compute(Time.deltaTime);
+        Draw();
+    }
+
+    public void Draw() 
+    {
+        Draw(vehicle.localAngularAcceleration, DebugGraph.DefaultBlue);
+        Draw(angularVelocityDifferentiation.localAngularAcceleration, DebugGraph.DefaultGreen);
+    }
+
+    public void Draw(Vector3 localAngularAcceleration, Color color)
+    {
+        DebugGraph.MultiLog("Yaw rad / s²", color, localAngularAcceleration.y);
+        DebugGraph.MultiLog("Roll rad / s²", color, localAngularAcceleration.z);
+        DebugGraph.MultiLog("Pitch rad / s²", color, localAngularAcceleration.x);
+    }
+}
---
+++ Assets/Scripts/Angular Acceleration/AngularVelocityDifferentiation.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class AngularVelocityDifferentiation
+{
+    public Rigidbody rb;
+
+    public Vector3 angularVelocity;
+    public Vector3 angularVelocity_prev;
+    public Vector3 angularAcceleration;
+    public Vector3 localAngularAcceleration;
+
+    public AngularVelocityDifferentiation(Rigidbody rb)
+    {
+        this.rb = rb;
+        angularVelocity = rb.angularVelocity;
+        angularVelocity_prev = angularVelocity;
+
+    }
+
+    public void Compute(float dt)
+    {
+        angularVelocity = rb.angularVelocity;
+        Vector3 diff = angularVelocity - angularVelocity_prev;
+        angularAcceleration = diff / dt;
+        localAngularAcceleration = rb.transform.InverseTransformDirection(angularAcceleration);
+
+        angularVelocity_prev = angularVelocity;
+    }
+}
