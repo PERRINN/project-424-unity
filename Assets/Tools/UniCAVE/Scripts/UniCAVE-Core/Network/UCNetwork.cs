cf02d96a461b12eef7977fa2f73d2fa6ab9d5a95
Edy <edytado@gmail.com>
2022-08-26 18:18:49 +0200

    UniCAVE: fixed arbitrarily playing all particle systems in the scene

--- Assets/Tools/UniCAVE/Scripts/UniCAVE-Core/Network/UCNetwork.cs
+++ Assets/Tools/UniCAVE/Scripts/UniCAVE-Core/Network/UCNetwork.cs
@@ -1,13 +1,13 @@
 ﻿//MIT License
-//Copyright 2016-Present 
+//Copyright 2016-Present
 //Ross Tredinnick
 //Benny Wysong-Grass
 //University of Wisconsin - Madison Virtual Environments Group
-//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
-//to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, 
+//Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
+//to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute,
 //sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
 //The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, 
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
 //INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
 //IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
 //TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
@@ -141,22 +141,27 @@ namespace UniCAVE
         void RpcSetRandomSeed(int seed)
         {
             ParticleSystem[] particleSystems = FindObjectsOfType<ParticleSystem>();
-            foreach(ParticleSystem ps in particleSystems)
+            bool[] isPlaying = new bool[particleSystems.Length];
+
+            for (int i = 0, c = particleSystems.Length; i < c; i++)
             {
+                ParticleSystem ps = particleSystems[i];
+                isPlaying[i] = ps.isPlaying;
                 ps.Stop();
             }
 
             Random.InitState(seed);
 
-            foreach(ParticleSystem ps in particleSystems)
+            for (int i = 0, c = particleSystems.Length; i < c; i++)
             {
+                ParticleSystem ps = particleSystems[i];
                 ps.useAutoRandomSeed = false;
                 ps.randomSeed = (uint)seed;
                 ps.Simulate(0.0f, true, true, false);
-                ps.Play();
+                if (isPlaying[i]) ps.Play();
             }
 
-            Debug.LogError($"Synced random seed to {seed}.");
+            Debug.Log($"Synced random seed to {seed}.");
         }
 
         /// <summary>
