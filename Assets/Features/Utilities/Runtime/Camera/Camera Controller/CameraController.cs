5a22dc98f9e8004f04ffb07d1e7eccf2e6125c03
Manuel Espino <espinolaramanuel@gmail.com>
2022-11-05 12:01:01 +0100

    Add Camera Controller

---
+++ Assets/Features/Utilities/Runtime/Camera/Camera Controller/CameraController.cs
@@ -0,0 +1,91 @@
+﻿using Cinemachine;
+using Perrinn424.Utilities;
+using UnityEngine;
+using UnityEngine.Assertions;
+using VehiclePhysics;
+
+namespace Perrinn424.CameraSystem
+{
+    public class CameraController : MonoBehaviour
+    {
+        public enum Mode
+        {
+            Driver,
+            SmoothFollow,
+            Orbit,
+            LookAt,
+            Free,
+            Tv
+        }
+
+
+        [SerializeField]
+        private Transform tvCameraSystem;
+        private CinemachineVirtualCameraBase cinemachineVirtualCameraBase;
+
+        [SerializeField]
+        private VPCameraController cameraController;
+
+
+        private CircularIterator<Mode> modeIterator;
+        public Vector3 microphoneLocalPosition;
+        private GameObject microphone;
+
+        private void Awake()
+        {
+            cinemachineVirtualCameraBase = tvCameraSystem.GetComponentInChildren<CinemachineVirtualCameraBase>();
+            Assert.IsNotNull(cinemachineVirtualCameraBase);
+        }
+
+        private void OnEnable()
+        {
+            modeIterator = new CircularIterator<Mode>(new[] { Mode.Driver, Mode.SmoothFollow, Mode.Orbit, Mode.LookAt, Mode.Free, Mode.Tv });
+            UpdateMode();
+        }
+
+        private void OnDisable()
+        {
+        }
+
+        public void NextMode()
+        {
+            modeIterator.MoveNext();
+            UpdateMode();
+        }
+
+        private void UpdateMode()
+        {
+            switch (modeIterator.Current)
+            {
+                case Mode.Driver:
+                case Mode.SmoothFollow:
+                case Mode.Orbit:
+                case Mode.LookAt:
+                case Mode.Free:
+                    cameraController.gameObject.SetActive(true);
+                    tvCameraSystem.gameObject.SetActive(false);
+                    RemoveMicrophone();
+                    cameraController.mode = (VPCameraController.Mode)modeIterator.Current;
+                    break;
+                case Mode.Tv:
+                    cameraController.gameObject.SetActive(false);
+                    tvCameraSystem.gameObject.SetActive(true);
+                    CreateMicrophone();
+                    break;
+            }
+        }
+
+        private void CreateMicrophone()
+        {
+            var player = cinemachineVirtualCameraBase.LookAt;
+            microphone = new GameObject("Microphone", new[] { typeof(AudioListener) });
+            microphone.transform.parent = player;
+            microphone.transform.localPosition = microphoneLocalPosition;
+        }
+
+        private void RemoveMicrophone()
+        {
+            Destroy(microphone);
+        }
+    } 
+}
