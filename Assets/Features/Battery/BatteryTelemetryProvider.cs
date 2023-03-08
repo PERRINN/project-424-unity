2d2e35bb19e0affd3e1273635840fb8ef8754446
Manuel Espino <espinolaramanuel@gmail.com>
2023-03-08 20:33:19 +0100

    Fix Telemetry

--- Assets/Features/Battery/BatteryTelemetryProvider.cs
+++ Assets/Features/Battery/BatteryTelemetryProvider.cs
@@ -11,7 +11,7 @@ namespace Perrinn424
 
 			public override int GetChannelCount()
 			{
-				return 1;
+				return 6;
 			}
 
 			public override void GetChannelInfo(Telemetry.ChannelInfo[] channelInfo, Object instance)
@@ -19,13 +19,28 @@ namespace Perrinn424
 				battery = (Battery)instance;
 
 
-				Telemetry.SemanticInfo temperatureSemantic = new Telemetry.SemanticInfo();
-				temperatureSemantic.SetRangeAndFormat(0, 70, "0", " degC");
+				Telemetry.SemanticInfo totalHeatSemantic = new Telemetry.SemanticInfo();
+				totalHeatSemantic.SetRangeAndFormat(0, 120000, "0", " W");
+				channelInfo[0].SetNameAndSemantic("BatteryTotalHeat", Telemetry.Semantic.Custom, totalHeatSemantic);
 
-				//PID info
-				channelInfo[0].SetNameAndSemantic("BatteryTemperature", Telemetry.Semantic.Custom, temperatureSemantic);
+                Telemetry.SemanticInfo airMassFlowSemantic = new Telemetry.SemanticInfo();
+                airMassFlowSemantic.SetRangeAndFormat(0, 5, "0.0", " kg/s");
+                channelInfo[1].SetNameAndSemantic("BatteryAirMassFlow", Telemetry.Semantic.Custom, airMassFlowSemantic);
 
-			}
+                Telemetry.SemanticInfo heatDissipationSemantic = new Telemetry.SemanticInfo();
+                heatDissipationSemantic.SetRangeAndFormat(0, 4000, "0.0", " W/degC");
+                channelInfo[2].SetNameAndSemantic("BatteryHeatDissipation", Telemetry.Semantic.Custom, heatDissipationSemantic);
+
+                Telemetry.SemanticInfo heatSemantic = new Telemetry.SemanticInfo();
+                heatSemantic.SetRangeAndFormat(-200, 200, "0.0", " J");
+                channelInfo[3].SetNameAndSemantic("BatteryHeatDissipated", Telemetry.Semantic.Custom, heatSemantic);
+
+                Telemetry.SemanticInfo temperatureSemantic = new Telemetry.SemanticInfo();
+                temperatureSemantic.SetRangeAndFormat(0, 75, "0.0", " degC");
+                channelInfo[4].SetNameAndSemantic("BatteryTemperature", Telemetry.Semantic.Custom, temperatureSemantic);
+
+                channelInfo[5].SetNameAndSemantic("BatteryQInternal", Telemetry.Semantic.Custom, heatSemantic);
+            }
 
 			public override Telemetry.PollFrequency GetPollFrequency()
 			{
@@ -34,8 +49,13 @@ namespace Perrinn424
 
 			public override void PollValues(float[] values, int index, Object instance)
 			{
-				values[index + 0] = battery.HeatInternal;
-			}
+				values[index + 0] = battery.TotalHeat;
+                values[index + 1] = battery.AirMassFlow;
+                values[index + 2] = battery.HeatDissipation;
+                values[index + 3] = battery.HeatDissipated;
+                values[index + 4] = battery.TemperatureModule;
+                values[index + 5] = battery.HeatInternal;
+            }
 		}
     } 
 }
