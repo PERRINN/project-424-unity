b4099343543d7ed4bf92b199be1e503c8940e0c1
Nicolas Perrin <nicolas@perrinn.com>
2024-04-01 17:24:51 +0200

    Tested and adjusted the active power balance gains and authority

--- Assets/Scripts/Perrinn424ActivePowerBalance.cs
+++ Assets/Scripts/Perrinn424ActivePowerBalance.cs
@@ -5,10 +5,8 @@ using System;
 
 public class Perrinn424ActivePowerBalance : VehicleBehaviour
 	{
-	[Range(0,1)]
 	public float rpmRatioFrontToRear = 1.0f;
 	public float gain = 0.0005f;
-	[Range(0,1)]
 	public float authority = 0.1f;
 	[Space(5)]
 	public bool emitTelemetry;
